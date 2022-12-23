62346b83933780a5b9ae3a41c192a93f40c92307
Davor Runje <davor@airt.ai>
2022-12-23 15:36:21 +0100

    Release candidate 0.0.1rc0

---
+++ mkdocs/docs_overrides/js/math.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
+
+
