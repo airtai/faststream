0adbd9e327ace40d7788d5fa8b33e49007c11986
Tvrtko Sternak <Tvrtko Sternak>
2023-02-06 07:27:29 +0000

    Remove unused testing methods

--- fastkafka/testing.py
+++ fastkafka/testing.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['logger', 'kafka_server_url', 'kafka_server_port', 'aiokafka_config', 'nb_safe_seed', 'true_after',
            'create_testing_topic', 'create_and_fill_testing_topic', 'mock_AIOKafkaProducer_send', 'change_dir',
-           'run_script_and_cancel', 'run_on_uvicorn']
+           'run_script_and_cancel']
 
 # %% ../nbs/999_Test_Utils.ipynb 1
 import asyncio
@@ -413,44 +413,3 @@ async def run_script_and_cancel(
             output, _ = proc.communicate()
 
         return (proc.returncode, output)
-
-# %% ../nbs/999_Test_Utils.ipynb 26
-async def run_on_uvicorn(
-    script: str,
-    *,
-    script_file: str = "server.py",
-    app_name: str = "app",
-    kafka_app_name: str = "kafka_app",
-    cmd: Optional[str] = None,
-    cancel_after: int = 60,
-    host: str = "127.0.0.1",
-    port: int = 8000,
-    workers: int = 1,
-) -> None:
-    """Runs a script using Uvicorn server
-    Args:
-        script: input Python script to save to temporary folder under the name `script_file` and import `app_name` from
-        script_file: name of the file for saving the input Python script
-        app_name: name of the variable in script holding the FastAPI object
-        cmd: command to execute
-        cancel_after: number of seconds to wait since the beginning of the execution before the TERMSIG is send to the Uvicorn application
-
-    Raises:
-
-    """
-    if cmd is None:
-        cmd = f"python3 -m uvicorn {Path(script_file).stem}:{app_name} --host {host} --port {port} --workers {workers}"
-
-    exit_code, output = await run_script_and_cancel(
-        script,
-        script_file=script_file,
-        cmd=cmd,
-        cancel_after=cancel_after,
-        app_name=app_name,
-        kafka_app_name=kafka_app_name,
-        generate_docs=True,
-    )
-
-    print(output.decode("utf-8"))
-    if exit_code != 0:
-        raise RuntimeError(f"Execution of '{cmd}' failed: {output.decode('utf-8')}")
