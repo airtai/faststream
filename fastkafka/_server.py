5286be81b0173e4679c3c38f41b2973654ea0639
Tvrtko Sternak <sternakt@gmail.com>
2023-03-10 12:20:21 +0000

    Checkout Server nb

--- fastkafka/_server.py
+++ fastkafka/_server.py
@@ -21,14 +21,13 @@ import typer
 from fastcore.basics import patch
 from fastcore.meta import delegates
 
-from ._application.app import FastKafka
-from ._components.logger import get_logger
-
 from fastkafka._components.helpers import (
     _import_from_string,
-    change_dir,
     generate_app_src,
+    change_dir,
 )
+from ._components.logger import get_logger
+from ._application.app import FastKafka
 
 # %% ../nbs/021_FastKafkaServer.ipynb 5
 logger = get_logger(__name__)
