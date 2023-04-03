5c13046dda7594825f673028b6c3197decafbce7
Hajdi Cenan <hajdi@airt.ai>
2023-04-03 14:26:19 +0200

    initial commit

--- docusaurus/src/components/HomepageFeatures/index.js
+++ docusaurus/src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Very easy to Use',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
