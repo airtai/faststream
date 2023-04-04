5b1435805ce07ee8ff40229cce10113d6b52c510
Harish Mohan Raj <harish@airt.ai>
2023-04-04 11:33:39 +0530

    Remove build_markdown_docs cli command (#197)
    * Remove build_markdwon_docs CLI command
    
    * Fix broken links
    
    * Fix broken links
    
    * Export the utility methods from the _docusaurus_helper file

--- docusaurus/scripts/build_docusaurus_docs.sh
+++ docusaurus/scripts/build_docusaurus_docs.sh
@@ -13,7 +13,7 @@ echo "Copy newly generated markdown files to docusaurus directory"
 cp -r mkdocs/docs docusaurus/
 
 echo "Generate API docs"
-build_markdown_docs
+python3 -c "from fastkafka._docusaurus_helper import fix_invalid_syntax_in_markdown, generate_markdown_docs; fix_invalid_syntax_in_markdown('./docusaurus/docs'); generate_markdown_docs('fastkafka', './docusaurus/docs')"
 
 echo "Run docusaurus build"
 cd docusaurus && npm run build
