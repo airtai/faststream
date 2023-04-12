44a53d3b7575d74835f562e97877147aed26bde6
Harish Mohan Raj <harish@airt.ai>
2023-04-12 11:06:13 +0530

    Add monochrome icons and github stars in documentation (#229)

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -84,6 +84,12 @@ const config = {
           },
 //           {to: '/blog', label: 'Blog', position: 'left'},
           {
+            type: 'html',
+            position: 'right',
+            className: 'github-stars',
+            value: '<iframe src="https://ghbtns.com/github-btn.html?user=airtai&repo=fastkafka&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>',
+          },
+          {
             href: 'https://github.com/airtai/fastkafka',
             position: 'right',
             className: "header-github-link",
