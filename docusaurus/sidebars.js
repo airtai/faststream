c1b0159e775c2e31ec6b35d1e7e931e0a465b3c6
Tvrtko Sternak <sternakt@gmail.com>
2023-04-28 13:18:22 +0000

    Add guide for Batch producing

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
 tutorialSidebar: [
-    'index', {'Guides': [{'Writing services': ['guides/Guide_11_Consumes_Basics', 'guides/Guide_21_Produces_Basics', 'guides/Guide_22_Partition_Keys', 'guides/Guide_05_Lifespan_Handler', 'guides/Guide_07_Encoding_and_Decoding_Messages_with_FastKafka']}, {'Testing': ['guides/Guide_31_Using_redpanda_to_test_fastkafka']}, {'Documentation generation': ['guides/Guide_04_Github_Actions_Workflow']}, {'Deployment': ['guides/Guide_30_Using_docker_to_deploy_fastkafka']}, {'Benchmarking': ['guides/Guide_06_Benchmarking_FastKafka']}]},
+    'index', {'Guides': [{'Writing services': ['guides/Guide_11_Consumes_Basics', 'guides/Guide_21_Produces_Basics', 'guides/Guide_22_Partition_Keys', 'guides/Guide_23_Batch_Producing', 'guides/Guide_05_Lifespan_Handler', 'guides/Guide_07_Encoding_and_Decoding_Messages_with_FastKafka']}, {'Testing': ['guides/Guide_31_Using_redpanda_to_test_fastkafka']}, {'Documentation generation': ['guides/Guide_04_Github_Actions_Workflow']}, {'Deployment': ['guides/Guide_30_Using_docker_to_deploy_fastkafka']}, {'Benchmarking': ['guides/Guide_06_Benchmarking_FastKafka']}]},
 //         [require("./docs/reference/sidebar.json")],
     {
         "items": [
