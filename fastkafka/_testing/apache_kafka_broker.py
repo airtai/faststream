17349558cccea53ab87634f00d13f3b39755b318
Kumaran Rajendhiran <kumaran@airt.ai>
2023-06-20 20:57:03 +0530

    Check port is free before starting zookeeper or kafka

--- fastkafka/_testing/apache_kafka_broker.py
+++ fastkafka/_testing/apache_kafka_broker.py
@@ -2,7 +2,7 @@
 
 # %% auto 0
 __all__ = ['logger', 'get_zookeeper_config_string', 'get_kafka_config_string', 'ApacheKafkaBroker', 'run_and_match',
-           'get_free_port', 'write_config_and_run']
+           'is_port_in_use', 'get_free_port', 'write_config_and_run']
 
 # %% ../../nbs/002_ApacheKafkaBroker.ipynb 1
 import asyncio
@@ -398,6 +398,20 @@ async def run_and_match(
     raise TimeoutError()
 
 # %% ../../nbs/002_ApacheKafkaBroker.ipynb 20
+def is_port_in_use(port: Union[int, str]) -> bool:
+    """
+    Checks if a port is already in use.
+
+    Args:
+        port (Union[int, str]): The port number to check. It can be provided as an integer or a string.
+
+    Returns:
+        bool: True if the port is in use, False otherwise.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(("localhost", int(port))) == 0
+
+# %% ../../nbs/002_ApacheKafkaBroker.ipynb 22
 def get_free_port() -> str:
     """Gets a port number which is available and free in the system.
 
@@ -484,6 +498,14 @@ async def _start_service(self: ApacheKafkaBroker, service: str = "kafka") -> Non
             )
 
         try:
+            port = (
+                self.zookeeper_kwargs["zookeeper_port"]
+                if service == "zookeeper"
+                else self.kafka_kwargs["listener_port"]
+            )
+            if is_port_in_use(port):
+                raise ValueError(f"Port {port} is already in use")
+
             script_extension = "bat" if platform.system() == "Windows" else "sh"
             service_start_script = f"{service}-server-start.{script_extension}"
             service_task = await run_and_match(
@@ -591,7 +613,7 @@ async def _stop(self: ApacheKafkaBroker) -> None:
     self.temporary_directory.__exit__(None, None, None)  # type: ignore
     self._is_started = False
 
-# %% ../../nbs/002_ApacheKafkaBroker.ipynb 23
+# %% ../../nbs/002_ApacheKafkaBroker.ipynb 25
 @patch
 def start(self: ApacheKafkaBroker) -> str:
     """Starts a local Kafka broker and ZooKeeper instance synchronously.
