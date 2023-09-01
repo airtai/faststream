b6f02d9331392ac377320edaa04a5e6fe660e8e5
“Robert <robert@airt.ai>
2023-09-01 15:28:25 +0200

    Update batch consumes test

--- docs_src/kafka/batch_consuming/app_and_test.py
+++ docs_src/kafka/batch_consuming/app_and_test.py
@@ -20,8 +20,14 @@ import pytest
 @pytest.mark.asyncio
 async def test_me():
     async with TestKafkaBroker(broker) as test_broker:
+        # This works
         await test_broker.publish("123", "test")
 
         # why is this failing?
         with pytest.raises(Exception) as e:
-            await test_broker.publish("123", "test_batch")
+            await test_broker.publish("I", "test_batch")
+            await test_broker.publish("am", "test_batch")
+            await test_broker.publish("FastStream", "test_batch")
+
+            # In the end we should be able to assert something like this
+            handle_batch.mock.assert_called_once_with(["I", "am", "FastStream"])
