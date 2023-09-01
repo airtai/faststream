57225cd683d4f4e7a7ff1e7946e2c3b34cf8206f
Sternakt <sternakt@gmail.com>
2023-09-01 10:59:46 +0200

    Fix mypy

--- faststream/utils/test_utils.py
+++ faststream/utils/test_utils.py
@@ -1,10 +1,10 @@
 import os
 import contextlib
 from pathlib import Path
-from typing import ContextManager
+from typing import Generator
 
 @contextlib.contextmanager
-def working_directory(path: str) -> ContextManager[None]:
+def working_directory(path: str) -> Generator[None, None, None]:
     """Changes working directory and returns to previous on exit."""
     prev_cwd = Path.cwd()
     os.chdir(path)
