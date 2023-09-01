1d10f3c29f47f2e992359d187eb841c988ed45cb
“Robert <robert@airt.ai>
2023-09-01 08:59:11 +0200

    Kafka producing example issue

---
+++ docs_src/kafka/problematic_example/testing.py
@@ -0,0 +1,17 @@
+import pytest
+
+from faststream.kafka import TestKafkaBroker
+from docs_src.kafka.problematic_example.app import (
+    broker, 
+    on_input_data,
+    to_input_data, 
+    Data,
+)
+
+@pytest.mark.asyncio
+async def test_base_app():
+    async with TestKafkaBroker(broker) as tester:
+        #await tester.publish(Data(data=0.2), "input_data")
+        await to_input_data(Data(data=0.2))
+        on_input_data.mock.assert_called_once_with(Data(data=5.2))
+        
