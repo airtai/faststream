d6e379105eba2fa1c3aab2a46ac8a6095103c17b
“Robert <robert@airt.ai>
2023-09-05 14:16:46 +0200

    Updte lifespan examples

--- docs_src/kafka/lifespan_events/app.py
+++ docs_src/kafka/lifespan_events/app.py
@@ -7,7 +7,7 @@ ml_models = {}  # fake ML model
 broker = KafkaBroker("localhost:9092")
 app = FastStream(broker)
 
-to_output_data = broker.publisher("output_data")
+predictions = broker.publisher("predictions_topic")
 
 
 def multiply(x: float) -> float:
@@ -27,9 +27,9 @@ async def shutdown_model():
     ml_models.clear()
 
 
-@to_output_data
+@predictions
 @broker.subscriber("input_data_1")
-async def predict_1(msg: float, logger: Logger) -> float:
+async def on_input_data_1(msg: float, logger: Logger) -> float:
     logger.info(f"{msg=}")
 
     result = ml_models["multiply_model"](msg)
@@ -39,48 +39,10 @@ async def predict_1(msg: float, logger: Logger) -> float:
 
 
 @broker.subscriber("input_data_2")
-async def predict_2(msg: float, logger: Logger) -> None:
+async def on_input_data_2(msg: float, logger: Logger) -> None:
     logger.info(f"{msg=}")
 
     result = ml_models["multiply_model"](msg)
     logger.info(f"{result=}")
     
-    await to_output_data.publish(result)
-
-
-@broker.subscriber("output_data")
-async def on_output_data(msg: float, logger: Logger):
-    pass
-
-import pytest
-
-from faststream.kafka import TestKafkaBroker
-from faststream import TestApp as T
-
-# from .basic import DataBasic, broker, on_input_data
-
-@pytest.mark.asyncio
-async def test_lifespan_with_publisher_decorator():
-    async with TestKafkaBroker(broker):
-        async with T(app):
-            
-            await broker.publish(2, "input_data_2")
-            # await predict_1.wait_call(2)
-        
-
-@pytest.mark.asyncio
-async def test_lifespan_with_await_inside_subscriber():
-
-
-    async with TestKafkaBroker(broker):
-        async with T(app):
-            
-            await broker.publish(2, "input_data_2")
-
-
-# @pytest.mark.asyncio
-# async def test_lifespan_with_await_inside_subscriber_without_tester():
-#     # async with TestKafkaBroker(broker):
-#     async with T(app):
-        
-#         await broker.publish(2, "input_data_2")
+    await predictions.publish(result)
