bff0bc7bf3d32af61567d1df83c987e42c21b7e5
“Robert <robert@airt.ai>
2023-09-05 13:57:07 +0200

    Update lifespan examples

--- docs_src/kafka/lifespan_events/app.py
+++ docs_src/kafka/lifespan_events/app.py
@@ -1,42 +1,16 @@
-from pydantic import BaseModel, Field, NonNegativeFloat
-
 from faststream import FastStream, Logger
 from faststream.kafka import KafkaBroker
-
-# from sklearn.datasets import load_iris
-# from sklearn.linear_model import LogisticRegression
-from contextlib import asynccontextmanager
-
-
-ml_models = {}
-
-# @asynccontextmanager
-# async def lifespan(app: FastKafka):
-#     # Load the ML model
-#     print("Loading the model!")
-#     X, y = load_iris(return_X_y=True)
-#     ml_models["iris_predictor"] = LogisticRegression(random_state=0, max_iter=500).fit(X, y)
-#     yield
-#     # Clean up the ML models and release the resources
-    
-#     print("Exiting, clearing model dict!")
-#     ml_models.clear()
-    
-
-
-class DataBasic(BaseModel):
-    data: NonNegativeFloat = Field(
-        ..., examples=[0.5], description="Float data example"
-    )
-
-
-broker = KafkaBroker("localhost:9092")
-app = FastStream(broker)
 
 
 ml_models = {}  # fake ML model
 
-def multiply(x: float):
+broker = KafkaBroker("localhost:9092")
+app = FastStream(broker)
+
+to_output_data = broker.publisher("output_data")
+
+
+def multiply(x: float) -> float:
     return x * 2
 
 @app.on_startup
@@ -44,10 +18,7 @@ async def setup_model():
     # Load the ML model
     print("Loading the model!")
     ml_models["multiply_model"] = multiply
-    # Load the ML model
-    # ml_models["answer_to_everything"] = fake_answer_to_everything_ml_model
-    # context.set_global("model", ml_models)
-    pass
+
 
 @app.on_shutdown
 async def shutdown_model():
@@ -55,32 +26,61 @@ async def shutdown_model():
     print("Exiting, clearing model dict!")
     ml_models.clear()
 
-@broker.subscriber("test")
-async def predict(msg: float, logger: Logger):
+
+@to_output_data
+@broker.subscriber("input_data_1")
+async def predict_1(msg: float, logger: Logger) -> float:
     logger.info(f"{msg=}")
+
     result = ml_models["multiply_model"](msg)
     logger.info(f"{result=}")
-    # result = model["answer_to_everything"](x)
-    # return {"result": result}
+    
+    return result
 
 
+@broker.subscriber("input_data_2")
+async def predict_2(msg: float, logger: Logger) -> None:
+    logger.info(f"{msg=}")
 
+    result = ml_models["multiply_model"](msg)
+    logger.info(f"{result=}")
+    
+    await to_output_data.publish(result)
+
+
+@broker.subscriber("output_data")
+async def on_output_data(msg: float, logger: Logger):
+    pass
 
 import pytest
 
 from faststream.kafka import TestKafkaBroker
-from faststream import TestApp
+from faststream import TestApp as T
 
 # from .basic import DataBasic, broker, on_input_data
 
-@broker.subscriber("output_data")
-async def on_output_data(msg: DataBasic):
-    pass
+@pytest.mark.asyncio
+async def test_lifespan_with_publisher_decorator():
+    async with TestKafkaBroker(broker):
+        async with T(app):
+            
+            await broker.publish(2, "input_data_2")
+            # await predict_1.wait_call(2)
+        
 
 @pytest.mark.asyncio
-async def test_base_app():
-        # pass
+async def test_lifespan_with_await_inside_subscriber():
+
+
     async with TestKafkaBroker(broker):
-        async with TestApp(app):
-            await broker.publish(2, "test")
+        async with T(app):
+            
+            await broker.publish(2, "input_data_2")
+
+
+# @pytest.mark.asyncio
+# async def test_lifespan_with_await_inside_subscriber_without_tester():
+#     # async with TestKafkaBroker(broker):
+#     async with T(app):
         
+#         await broker.publish(2, "input_data_2")
