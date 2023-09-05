6bb5c15ee88f7de23b4155834d5c8970e2d579c0
Davor Runje <davor@airt.ai>
2023-09-05 16:00:05 +0200

    polish docs

--- docs_src/kafka/raw_publish/test_me.py
+++ docs_src/kafka/raw_publish/test_me.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel, Field, NonNegativeFloat
 from faststream import FastStream, Logger
 from faststream.kafka import KafkaBroker, TestKafkaBroker
 
+from typing import Any, Dict
+
 broker = KafkaBroker("localhost:9092")
 app = FastStream(broker)
 
@@ -14,9 +16,17 @@ class Data(BaseModel):
     )
 
 
+# @broker.subscriber("input_data")
+# async def handle_dict(msg: Dict[str, Any], logger: Logger) -> None:
+#     logger.info(f"handle_dict({msg=})")
+
 @broker.subscriber("input_data")
-async def handle(msg: Data, logger: Logger) -> None:
-    logger.info(f"handle({msg=})")
+async def handle_data(msg: Data, logger: Logger) -> None:
+    logger.info(f"handle_data({msg=})")
+
+# @broker.subscriber("input_data")
+# async def handle_bytes(msg: bytes, logger: Logger) -> None:
+#     logger.info(f"handle_bytes({msg=})")
 
 
 @pytest.mark.asyncio
@@ -25,6 +35,7 @@ async def test_raw_publish():
     async with TestKafkaBroker(broker):
         msg = Data(data=0.5)
 
-        await broker.publish(msg.model_dump_json().encode("utf-8"), "input_data")
+        # await broker.publish(msg, "input_data)")
+        await broker.publish(msg.model_dump_json().encode("utf-8"), "input_data", headers={"Content-Type": "application/json"})
 
         # handle.mock.assert_called_once_with(Data(data=0.5))
