dc6edae0458185630abf923e2023440f7196ec9c
Pastukhov Nikita <diementros@yandex.ru>
2023-09-16 01:23:35 +0300

    refactor: editable Context objects (#653)

--- tests/faststream_gen_examples/test_example_infinity_publishing.py
+++ tests/faststream_gen_examples/test_example_infinity_publishing.py
@@ -1,5 +1,7 @@
+import pytest
+
 from faststream_gen_examples.example_infinity_publishing.test_app import (
     test_message_was_published,
 )
 
-__all__ = ("test_message_was_published",)
+pytest.mark.slow(test_message_was_published)
