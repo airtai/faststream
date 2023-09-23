d5a3a1f91e2050b9cb4821cb5e8b8f0668ae8aac
Lancetnik <diementros@yandex.ru>
2023-09-23 19:01:44 +0300

    test: mark pydantic_settings with pydanticV2

--- tests/marks.py
+++ tests/marks.py
@@ -3,3 +3,5 @@ import pytest
 from faststream._compat import PYDANTIC_V2
 
 pydanticV1 = pytest.mark.skipif(PYDANTIC_V2, reason="requires PydanticV2")
+
+pydanticV2 = pytest.mark.skipif(not PYDANTIC_V2, reason="requires PydanticV1")
