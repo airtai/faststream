b6e8758cca1bf2da8115c1def8ef111f23bab4bc
Lancetnik <diementros@yandex.ru>
2023-09-23 19:09:46 +0300

    test: correct cli tests

--- tests/docs/getting_started/cli/test_kafka_context.py
+++ tests/docs/getting_started/cli/test_kafka_context.py
@@ -1,10 +1,17 @@
 import pytest
 
-from docs.docs_src.getting_started.cli.kafka_context import app
 from faststream import TestApp, context
+from faststream.kafka import TestKafkaBroker
+from tests.marks import pydanticV2
+from tests.mocks import mock_pydantic_settings_env
 
 
+@pydanticV2
 @pytest.mark.asyncio
 async def test():
-    async with TestApp(app, {"env": ""}):
-        assert context.get("settings").host == "localhost:9092"
+    with mock_pydantic_settings_env({"host": "localhost"}):
+        from docs.docs_src.getting_started.cli.kafka_context import app, broker
+
+        async with TestKafkaBroker(broker):
+            async with TestApp(app, {"env": ""}):
+                assert context.get("settings").host == "localhost"
--- tests/docs/getting_started/cli/test_rabbit_context.py
+++ tests/docs/getting_started/cli/test_rabbit_context.py
@@ -1,10 +1,19 @@
 import pytest
 
-from docs.docs_src.getting_started.cli.rabbit_context import app
 from faststream import TestApp, context
+from faststream.rabbit import TestRabbitBroker
+from tests.marks import pydanticV2
+from tests.mocks import mock_pydantic_settings_env
 
 
+@pydanticV2
 @pytest.mark.asyncio
 async def test():
-    async with TestApp(app, {"env": ""}):
-        assert context.get("settings").host == "amqp://guest:guest@localhost:5672/"
+    with mock_pydantic_settings_env({"host": "amqp://guest:guest@localhost:5673/"}):
+        from docs.docs_src.getting_started.cli.rabbit_context import app, broker
+
+        async with TestRabbitBroker(broker):
+            async with TestApp(app, {"env": ".env"}):
+                assert (
+                    context.get("settings").host == "amqp://guest:guest@localhost:5673/"
+                )
