aafb5866abf5abd3c90cf03465c53a377b3b4cc2
Lancetnik <diementros@yandex.ru>
2023-09-24 17:52:09 +0300

    test: cover docs_src extra dirs

---
+++ tests/docs/getting_started/subscription/test_testing.py
@@ -0,0 +1,19 @@
+from docs.docs_src.getting_started.subscription.testing_kafka import (
+    test_handle as test_handle_k,
+)
+from docs.docs_src.getting_started.subscription.testing_kafka import (
+    test_validation_error as test_validation_error_k,
+)
+from docs.docs_src.getting_started.subscription.testing_rabbit import (
+    test_handleas as test_handle_r,
+)
+from docs.docs_src.getting_started.subscription.testing_rabbit import (
+    test_validation_error as test_validation_error_r,
+)
+
+__all__ = (
+    "test_handle_r",
+    "test_validation_error_r",
+    "test_handle_k",
+    "test_validation_error_k",
+)
