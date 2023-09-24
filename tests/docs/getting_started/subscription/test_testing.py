d1d47087de3476db446fce42483fe16c68531d5c
Lancetnik <diementros@yandex.ru>
2023-09-24 17:54:42 +0300

    fix typo

--- tests/docs/getting_started/subscription/test_testing.py
+++ tests/docs/getting_started/subscription/test_testing.py
@@ -5,7 +5,7 @@ from docs.docs_src.getting_started.subscription.testing_kafka import (
     test_validation_error as test_validation_error_k,
 )
 from docs.docs_src.getting_started.subscription.testing_rabbit import (
-    test_handleas as test_handle_r,
+    test_handle as test_handle_r,
 )
 from docs.docs_src.getting_started.subscription.testing_rabbit import (
     test_validation_error as test_validation_error_r,
