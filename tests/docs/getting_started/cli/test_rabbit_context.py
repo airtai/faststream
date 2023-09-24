ba3cdf98e2f1ac168ca0c1796be889e22b0c88d4
Davor Runje <davor@airt.ai>
2023-09-24 18:29:44 +0200

    library to framework changed

--- tests/docs/getting_started/cli/test_rabbit_context.py
+++ tests/docs/getting_started/cli/test_rabbit_context.py
@@ -9,11 +9,14 @@ from tests.mocks import mock_pydantic_settings_env
 @pydanticV2
 @pytest.mark.asyncio
 async def test():
-    with mock_pydantic_settings_env({"host": "amqp://guest:guest@localhost:5673/"}):
+    with mock_pydantic_settings_env(
+        {"host": "amqp://guest:guest@localhost:5673/"}  # pragma: allowlist secret
+    ):
         from docs.docs_src.getting_started.cli.rabbit_context import app, broker
 
         async with TestRabbitBroker(broker):
             async with TestApp(app, {"env": ".env"}):
                 assert (
-                    context.get("settings").host == "amqp://guest:guest@localhost:5673/"
+                    context.get("settings").host
+                    == "amqp://guest:guest@localhost:5673/"  # pragma: allowlist secret
                 )
