6498b7b365b45cc061a3202ede95677e12481e50
Lancetnik <diementros@yandex.ru>
2023-09-24 15:34:16 +0300

    test: cover index docs_src

---
+++ tests/docs/index/test_pydantic.py
@@ -0,0 +1,11 @@
+from docs.docs_src.index.test_kafka import test_correct as test_k_correct
+from docs.docs_src.index.test_kafka import test_invalid as test_k_invalid
+from docs.docs_src.index.test_rabbit import test_correct as test_r_correct
+from docs.docs_src.index.test_rabbit import test_invalid as test_r_invalid
+
+__all__ = (
+    "test_k_correct",
+    "test_k_invalid",
+    "test_r_correct",
+    "test_r_invalid",
+)
