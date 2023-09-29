762ba8f8bb0b64a6697071ea863eee769781bca7
Sternakt <sternakt@gmail.com>
2023-09-29 12:47:52 +0200

    Fix referencing wrong test in publish_raw

--- tests/docs/kafka/raw_publish/test_raw_publish.py
+++ tests/docs/kafka/raw_publish/test_raw_publish.py
@@ -1,3 +1,3 @@
-from docs.docs_src.kafka.publisher_object.example import test_prepared_publish
+from docs.docs_src.kafka.raw_publish.example import test_prepared_publish
 
 __all__ = ["test_prepared_publish"]
