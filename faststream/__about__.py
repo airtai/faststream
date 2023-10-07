ec4d30ac1126617cb3124bd2ab34dea10569ad8a
Lancetnik <diementros@yandex.ru>
2023-10-07 22:01:30 +0300

    chore: trigger tests

--- faststream/__about__.py
+++ faststream/__about__.py
@@ -1,7 +1,6 @@
 """Simple and fast framework to create message brokers based microservices"""
 __version__ = "0.2.3"
 
-
 INSTALL_YAML = """
 To generate YAML documentation, please install dependencies:\n
 pip install PyYAML
