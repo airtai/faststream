2f5d556ab801681d6319db409536436e131c56c7
Lancetnik <diementros@yandex.ru>
2023-10-09 23:06:19 +0300

    feat: fast access to Message annotations

---
+++ faststream/nats/__init__.py
@@ -0,0 +1,45 @@
+from nats.js.api import (
+    AckPolicy,
+    ConsumerConfig,
+    DeliverPolicy,
+    DiscardPolicy,
+    ExternalStream,
+    Placement,
+    ReplayPolicy,
+    RePublish,
+    RetentionPolicy,
+    StorageType,
+    StreamConfig,
+    StreamSource,
+)
+
+from faststream.broker.test import TestApp
+from faststream.nats.broker import NatsBroker
+from faststream.nats.js_stream import JStream
+from faststream.nats.annotations import NatsMessage
+from faststream.nats.router import NatsRouter
+from faststream.nats.shared.router import NatsRoute
+from faststream.nats.test import TestNatsBroker
+
+__all__ = (
+    "TestApp",
+    "TestNatsBroker",
+    "NatsMessage",
+    "NatsBroker",
+    "NatsRouter",
+    "NatsRoute",
+    "JStream",
+    # Nats imports
+    "ConsumerConfig",
+    "DeliverPolicy",
+    "AckPolicy",
+    "ReplayPolicy",
+    "DiscardPolicy",
+    "RetentionPolicy",
+    "ExternalStream",
+    "Placement",
+    "RePublish",
+    "StorageType",
+    "StreamConfig",
+    "StreamSource",
+)
