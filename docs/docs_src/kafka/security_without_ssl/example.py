0769db9818f93b2d7051f380f106548f0f421db9
Sternakt <sternakt@gmail.com>
2023-10-17 11:20:06 +0200

    Fix use_ssl flag test

--- docs/docs_src/kafka/security_without_ssl/example.py
+++ docs/docs_src/kafka/security_without_ssl/example.py
@@ -6,6 +6,6 @@ from faststream.broker.security import SASLPlaintext, ssl_not_set_error_msg
 def test_without_ssl_warning():
     with pytest.raises(RuntimeError) as excinfo:
         security = SASLPlaintext(username="admin", password="password")
-    assert str(excinfo) == ssl_not_set_error_msg
+    assert str(excinfo.value) == ssl_not_set_error_msg, excinfo.value
 
     security = SASLPlaintext(username="admin", password="password", use_ssl=False)
