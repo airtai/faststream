66c33f6b58f5ef3a0d837329b9161569c26346cb
Nikita Pastukhov <diementros@yandex.ru>
2023-11-03 16:41:31 +0300

    fix (#910): correct pydantic enum refs resolving

--- tests/asyncapi/base/arguments.py
+++ tests/asyncapi/base/arguments.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Optional, Type
 
 import pydantic
@@ -225,6 +226,44 @@ class FastAPICompatible:
                 "title": key,
                 "type": "object",
             }
+
+    def test_pydantic_model_with_enum(self):
+        class Status(str, Enum):
+            registered = "registered"
+            banned = "banned"
+
+        class User(pydantic.BaseModel):
+            name: str = ""
+            id: int
+            status: Status
+
+        broker = self.broker_class()
+
+        @broker.subscriber("test")
+        async def handle(user: User):
+            ...
+
+        schema = get_app_schema(self.build_app(broker)).to_jsonable()
+
+        payload = schema["components"]["schemas"]
+
+        assert payload == {
+            "Status": {
+                "enum": ["registered", "banned"],
+                "title": "Status",
+                "type": "string",
+            },
+            "User": {
+                "properties": {
+                    "id": {"title": "Id", "type": "integer"},
+                    "name": {"default": "", "title": "Name", "type": "string"},
+                    "status": {"$ref": "#/components/schemas/Status"},
+                },
+                "required": ["id", "status"],
+                "title": "User",
+                "type": "object",
+            },
+        }
 
     def test_pydantic_model_mixed_regular(self):
         class Email(pydantic.BaseModel):
