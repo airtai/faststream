0ad12bf7521d6c8d5c97b59bcb76d1041904b60b
sheldy <chikibamtioma@gmail.com>
2023-11-05 14:53:49 +0200

    fix: correct `PullSub` stub file

--- faststream/nats/pull_sub.pyi
+++ faststream/nats/pull_sub.pyi
@@ -1,53 +1,12 @@
-from typing import List, Optional
+from typing import Optional
 
-from nats.js.api import (
-    DiscardPolicy,
-    Placement,
-    RePublish,
-    RetentionPolicy,
-    StorageType,
-    StreamConfig,
-    StreamSource,
-)
-from pydantic import Field
+from pydantic import BaseModel, Field
 
-from faststream.broker.schemas import NameRequired
-
-class PullSub(NameRequired):
-    config: StreamConfig
-
-    subjects: List[str] = Field(default_factory=list)
-    declare: bool = Field(default=True)
-
+class PullSub(BaseModel):
+    batch_size: int = Field(default=1)
+    timeout: Optional[float] = Field(default=5.0)
     def __init__(
         self,
-        name: str,
         batch_size: int,
-        description: Optional[str] = None,
-        subjects: Optional[List[str]] = None,
-        retention: Optional[RetentionPolicy] = None,
-        max_consumers: Optional[int] = None,
-        max_msgs: Optional[int] = None,
-        max_bytes: Optional[int] = None,
-        discard: Optional[DiscardPolicy] = DiscardPolicy.OLD,
-        max_age: Optional[float] = None,  # in seconds
-        max_msgs_per_subject: int = -1,
-        max_msg_size: Optional[int] = -1,
-        storage: Optional[StorageType] = None,
-        num_replicas: Optional[int] = None,
-        no_ack: bool = False,
-        template_owner: Optional[str] = None,
-        duplicate_window: float = 0,
-        placement: Optional[Placement] = None,
-        mirror: Optional[StreamSource] = None,
-        sources: Optional[List[StreamSource]] = None,
-        sealed: bool = False,
-        deny_delete: bool = False,
-        deny_purge: bool = False,
-        allow_rollup_hdrs: bool = False,
-        republish: Optional[RePublish] = None,
-        allow_direct: Optional[bool] = None,
-        mirror_direct: Optional[bool] = None,
-        # custom
-        declare: bool = True,
+        timeout: float
     ) -> None: ...
