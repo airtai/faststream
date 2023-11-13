f483d11c73a20c7f7baee123688e967890e5d7fd
Pastukhov Nikita <diementros@yandex.ru>
2023-11-13 18:05:15 +0300

    docs: fix update_release script (#945)

--- docs/update_releases.py
+++ docs/update_releases.py
@@ -1,8 +1,29 @@
 import re
 from pathlib import Path
+from typing import List, Tuple
 
 import requests
 import typer
+
+
+def find_metablock(lines: List[str]) -> Tuple[List[str], List[str]]:
+    if lines[0] != "---":
+        return [], lines
+
+    index: int = 0
+    for i in range(1, len(lines)):
+        if lines[i] == "---":
+            index = i + 1
+
+    return lines[:index], lines[index:]
+
+
+def find_header(lines: List[str]) -> Tuple[str, List[str]]:
+    for i in range(len(lines)):
+        if (line := lines[i]).startswith("#"):
+            return line, lines[i+1:]
+
+    return "", lines
 
 
 def get_github_releases():
@@ -24,14 +45,17 @@ def convert_links_and_usernames(text):
 
 def update_release_notes(realease_notes_path: Path):
     typer.echo("Updating Release Notes")
+
     releases = get_github_releases()
 
     # Get the changelog from the RELEASE.md file
-    with open(realease_notes_path, "r") as f:
-        changelog = f.read()
+    changelog = realease_notes_path.read_text()
 
-    lines = changelog.splitlines()
-    header, changelog = "\n".join(lines[0:8]), "\n".join(lines[8:])
+    metablock, lines = find_metablock(changelog.splitlines())
+    metablock = "\n".join(metablock)
+
+    header, changelog = find_header(lines)
+    changelog = "\n".join(changelog)
 
     for release in reversed(releases):
         version = release["tag_name"]
@@ -44,6 +68,13 @@ def update_release_notes(realease_notes_path: Path):
             changelog = version_changelog + changelog
 
     # Update the RELEASE.md file with the latest version and changelog
-    with open(realease_notes_path, "w") as f:
-        release_notes = (header + "\n" + changelog + "\n").replace('\r','')
-        f.write(release_notes)
+    realease_notes_path.write_text((
+        metablock + "\n\n" +
+        header + "\n\n" +
+        changelog
+    ).replace("\r", ""))
+
+
+if __name__ == "__main__":
+    base_dir = Path(__file__).resolve().parent
+    update_release_notes(base_dir / "docs" / "en" / "release.md")
