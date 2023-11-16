9fa7ddd99c404417aeed5012049c44d4a18a93e6
Kumaran Rajendhiran <kumaran@airt.ai>
2023-11-16 20:37:22 +0530

    fix: usage pass apps module rather than file path (#955)
    * Use uvicorn style importer to import app from string
    
    * Lint
    
    * Fix static analysis issues
    
    * Use typer's exception class
    
    * Update docs
    
    * Fix using old method to import from string
    
    * Bump version to 0.2.14

--- tests/cli/utils/test_imports.py
+++ tests/cli/utils/test_imports.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 import pytest
+from typer import BadParameter
 
-from faststream.cli.utils.imports import get_app_path, import_object
+from faststream.app import FastStream
+from faststream.cli.utils.imports import get_app_path, import_from_string, import_object
 
 
 def test_import_wrong():
@@ -27,3 +29,27 @@ def test_get_app_path(test_input, exp_module, exp_app):
 def test_get_app_path_wrong():
     with pytest.raises(ValueError):
         get_app_path("module.app")
+
+
+def test_import_from_string_import_wrong():
+    with pytest.raises(BadParameter):
+        module, app = import_from_string("tests:test_object")
+
+
+@pytest.mark.parametrize(
+    "test_input,exp_module",
+    (
+        ("examples.kafka.testing:app", "examples/kafka/testing.py"),
+        ("examples.nats.e01_basic:app", "examples/nats/e01_basic.py"),
+        ("examples.rabbit.topic:app", "examples/rabbit/topic.py"),
+    ),
+)
+def test_import_from_string(test_input, exp_module):
+    module, app = import_from_string(test_input)
+    assert isinstance(app, FastStream)
+    assert module == (Path.cwd() / exp_module)
+
+
+def test_import_from_string_wrong():
+    with pytest.raises(BadParameter):
+        import_from_string("module.app")
