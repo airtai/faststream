f95a7658cc6aa7a064222f6ee7011f0c8985bfd9
Kumaran Rajendhiran <kumaran@airt.ai>
2023-11-16 18:13:22 +0530

    Use uvicorn style importer to import app from string

--- tests/cli/utils/test_imports.py
+++ tests/cli/utils/test_imports.py
@@ -2,28 +2,29 @@ from pathlib import Path
 
 import pytest
 
-from faststream.cli.utils.imports import get_app_path, import_object
+from faststream.cli.utils.imports import import_from_string, ImportFromStringError
+from faststream.app import FastStream
 
 
 def test_import_wrong():
-    dir, app = get_app_path("tests:test_object")
-    with pytest.raises(FileNotFoundError):
-        import_object(dir, app)
+    with pytest.raises(ImportFromStringError):
+        module, app = import_from_string("tests:test_object")
 
 
 @pytest.mark.parametrize(
-    "test_input,exp_module,exp_app",
+    "test_input,exp_module",
     (
-        ("module:app", "module", "app"),
-        ("module.module.module:app", "module/module/module", "app"),
+        ("examples.kafka.testing:app", "examples/kafka/testing.py"),
+        ("examples.nats.e01_basic:app", "examples/nats/e01_basic.py"),
+        ("examples.rabbit.topic:app", "examples/rabbit/topic.py"),
     ),
 )
-def test_get_app_path(test_input, exp_module, exp_app):
-    dir, app = get_app_path(test_input)
-    assert app == exp_app
-    assert dir == Path.cwd() / exp_module
+def test_get_app_path(test_input, exp_module):
+    module, app = import_from_string(test_input)
+    assert isinstance(app, FastStream)
+    assert Path(module.__file__) == (Path.cwd() / exp_module)
 
 
 def test_get_app_path_wrong():
-    with pytest.raises(ValueError):
-        get_app_path("module.app")
+    with pytest.raises(ImportFromStringError):
+        import_from_string("module.app")
