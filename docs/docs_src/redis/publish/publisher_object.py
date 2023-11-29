1ff050407facd2b95239f78b3291fa315a473aa4
Kumaran Rajendhiran <kumaran@airt.ai>
2023-11-29 14:36:52 +0530

    Add redis channel publish documentation

---
+++ docs/docs_src/redis/publish/publisher_object.py
@@ -0,0 +1,33 @@
+import pytest
+from pydantic import BaseModel, Field, NonNegativeFloat
+
+from faststream import FastStream, Logger
+from faststream._compat import model_to_json
+from faststream.redis import RedisBroker, TestRedisBroker
+
+broker = RedisBroker("localhost:6379")
+app = FastStream(broker)
+
+
+class Data(BaseModel):
+    data: NonNegativeFloat = Field(
+        ..., examples=[0.5], description="Float data example"
+    )
+
+prepared_publisher = broker.publisher("input_data")
+
+@broker.subscriber("input_data")
+async def handle_data(msg: Data, logger: Logger) -> None:
+    logger.info(f"handle_data({msg=})")
+
+@pytest.mark.asyncio
+async def test_prepared_publish():
+    async with TestRedisBroker(broker):
+        msg = Data(data=0.5)
+
+        await prepared_publisher.publish(
+            model_to_json(msg),
+            headers={"content-type": "application/json"},
+        )
+
+        handle_data.mock.assert_called_once_with(dict(msg))
