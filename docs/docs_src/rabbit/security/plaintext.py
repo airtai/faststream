052542b0d1dcd40dd4e340dcbb0c6c921c5e4bff
Davor Runje <davor@airt.ai>
2023-12-15 09:59:37 +0100

    update precommit

--- docs/docs_src/rabbit/security/plaintext.py
+++ docs/docs_src/rabbit/security/plaintext.py
@@ -7,7 +7,7 @@ ssl_context = ssl.create_default_context()
 security = SASLPlaintext(
     ssl_context=ssl_context,
     username="admin",
-    password="password",
+    password="password",  # pragma: allowlist secret
 )
 
 broker = RabbitBroker(security=security)
