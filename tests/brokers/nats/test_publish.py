f573af6b210aa310235978abe93caa737bb7b87a
Pastukhov Nikita <diementros@yandex.ru>
2023-12-20 21:52:57 +0300

    fix (#1082): correct NatsTestClient stream publisher (#1083)
    * fix (#1082): correct NatsTestClient stream publisher
    
    * chore: remove anyio restriction
    
    * test: add test for ExceptionGroup
    
    * test: fix compatibility

--- tests/brokers/nats/test_publish.py
+++ tests/brokers/nats/test_publish.py
@@ -5,4 +5,30 @@ from tests.brokers.base.publish import BrokerPublishTestcase
 
 @pytest.mark.nats
 class TestPublish(BrokerPublishTestcase):
-    pass
+    @pytest.mark.asyncio
+    async def test_stream_publish(
+        self,
+        queue: str,
+        test_broker,
+    ):
+        @test_broker.subscriber(queue, stream="test")
+        async def m():
+            ...
+
+        await test_broker.start()
+        await test_broker.publish("Hi!", queue, stream="test")
+        m.mock.assert_called_once_with("Hi!")
+
+    @pytest.mark.asyncio
+    async def test_wrong_stream_publish(
+        self,
+        queue: str,
+        test_broker,
+    ):
+        @test_broker.subscriber(queue)
+        async def m():
+            ...
+
+        await test_broker.start()
+        await test_broker.publish("Hi!", queue, stream="test")
+        assert not m.mock.called
