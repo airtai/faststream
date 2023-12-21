9a5f867bb88df3e5869838547597fb8d821ec793
Davor Runje <davor@airt.ai>
2023-12-21 15:36:26 +0100

    linting...

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 echo "Running pyup_dirs..."
 pyup_dirs --py38-plus --recursive faststream examples tests docs
 
 echo "Running ruff linter (isort, flake, pyupgrade, etc. replacement)..."
-ruff check
+ruff check --exit-non-zero-on-fix
 
 echo "Running ruff formater (black replacement)..."
 ruff format
