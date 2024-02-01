96006adc5ae90129e195971b43570c20c35e5719
Nikita Pastukhov <diementros@yandex.ru>
2024-02-01 22:11:09 +0300

    fix mypy

--- faststream/broker/publisher.py
+++ faststream/broker/publisher.py
@@ -117,7 +117,7 @@ class BasePublisher(AsyncAPIOperation, Generic[MsgType]):
             call_model: CallModel[Any, Any] = CallModel(
                 call=lambda: None,
                 model=create_model("Fake"),
-                response_model=create_model(
+                response_model=create_model(  # type: ignore[call-overload]
                     "",
                     __config__=get_config_base(),  # type: ignore[arg-type]
                     **params,  # type: ignore[arg-type]
