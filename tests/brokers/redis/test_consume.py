784e24a895357ce5305bbe61ec9beb875176e348
Davor Runje <davor@airt.ai>
2024-04-07 06:03:32 +0200

    docs and linter fixes

--- tests/brokers/redis/test_consume.py
+++ tests/brokers/redis/test_consume.py
@@ -226,30 +226,32 @@ class TestConsumeStream:
 
         mock.assert_called_once_with("hello")
 
-    @pytest.mark.slow()
-    @pytest.mark.asyncio()
-    async def test_consume_group(
-        self,
-        queue: str,
-        full_broker: RedisBroker,
-    ):
-        @full_broker.subscriber(stream=StreamSub(queue, group="group", consumer=queue))
-        async def handler(msg: RedisMessage): ...
+    # commented out because it is already defined below
+    # @pytest.mark.slow()
+    # @pytest.mark.asyncio()
+    # async def test_consume_group(
+    #     self,
+    #     queue: str,
+    #     full_broker: RedisBroker,
+    # ):
+    #     @full_broker.subscriber(stream=StreamSub(queue, group="group", consumer=queue))
+    #     async def handler(msg: RedisMessage): ...
 
-        assert next(iter(full_broker.handlers.values())).last_id == "$"
+    #     assert next(iter(full_broker.handlers.values())).last_id == "$"
 
-    @pytest.mark.asyncio()
-    async def test_consume_group_with_last_id(
-        self,
-        queue: str,
-        full_broker: RedisBroker,
-    ):
-        @full_broker.subscriber(
-            stream=StreamSub(queue, group="group", consumer=queue, last_id="0")
-        )
-        async def handler(msg: RedisMessage): ...
+    # commented out because it is already defined below
+    # @pytest.mark.asyncio()
+    # async def test_consume_group_with_last_id(
+    #     self,
+    #     queue: str,
+    #     full_broker: RedisBroker,
+    # ):
+    #     @full_broker.subscriber(
+    #         stream=StreamSub(queue, group="group", consumer=queue, last_id="0")
+    #     )
+    #     async def handler(msg: RedisMessage): ...
 
-        assert next(iter(full_broker.handlers.values())).last_id == "0"
+    #     assert next(iter(full_broker.handlers.values())).last_id == "0"
 
     @pytest.mark.slow()
     async def test_consume_stream_native(
@@ -333,29 +335,27 @@ class TestConsumeStream:
 
         mock.assert_called_once_with([{"message": "hello"}])
 
-    # commented out because it is already defined above
-    # async def test_consume_group(
-    #     self,
-    #     queue: str,
-    #     full_broker: RedisBroker,
-    # ):
-    #     @full_broker.subscriber(stream=StreamSub(queue, group="group", consumer=queue))
-    #     async def handler(msg: RedisMessage):
-    #         ...
+    async def test_consume_group(
+        self,
+        queue: str,
+        full_broker: RedisBroker,
+    ):
+        @full_broker.subscriber(stream=StreamSub(queue, group="group", consumer=queue))
+        async def handler(msg: RedisMessage): ...
 
-    #     assert next(iter(full_broker._subscribers.values())).last_id == "$"
+        assert next(iter(full_broker._subscribers.values())).last_id == "$"
 
-    # commented out because it is already defined above
-    # async def test_consume_group_with_last_id(
-    #     self,
-    #     queue: str,
-    #     full_broker: RedisBroker,
-    # ):
-    #     @full_broker.subscriber(stream=StreamSub(queue, group="group", consumer=queue, last_id="0"))
-    #     async def handler(msg: RedisMessage):
-    #         ...
+    async def test_consume_group_with_last_id(
+        self,
+        queue: str,
+        full_broker: RedisBroker,
+    ):
+        @full_broker.subscriber(
+            stream=StreamSub(queue, group="group", consumer=queue, last_id="0")
+        )
+        async def handler(msg: RedisMessage): ...
 
-    #     assert next(iter(full_broker._subscribers.values())).last_id == "0"
+        assert next(iter(full_broker._subscribers.values())).last_id == "0"
 
     async def test_consume_nack(
         self,
