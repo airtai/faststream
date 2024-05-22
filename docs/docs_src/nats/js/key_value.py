609abac2e8c272b4b065d58cc8818ac449a6e274
Nikita Pastukhov <diementros@yandex.ru>
2024-05-23 00:00:15 +0300

    docs: update nats kv and os documentation

--- docs/docs_src/nats/js/key_value.py
+++ docs/docs_src/nats/js/key_value.py
@@ -1,33 +1,16 @@
-from nats.js.kv import KeyValue as KV
-from typing_extensions import Annotated
-
-from faststream import Logger
-from faststream import Context, FastStream, Logger
+from faststream import FastStream
 from faststream.nats import NatsBroker
-from faststream.nats.annotations import ContextRepo
-
-KeyValue = Annotated[KV, Context("kv")]
 
 broker = NatsBroker()
 app = FastStream(broker)
 
 
-@broker.subscriber("subject")
-async def handler(msg: str, kv: KeyValue, logger: Logger):
-    logger.info(msg)
-    kv_data = await kv.get("key")
-    assert kv_data.value == b"Hello!"
-
-
-@app.on_startup
-async def setup_broker(context: ContextRepo):
-    await broker.connect()
-
-    kv = await broker.stream.create_key_value(bucket="bucket")
-    context.set_global("kv", kv)
+@broker.subscriber("key", kv_watch="bucket")
+async def handler(msg: str):
+    assert msg == "Hello!"
 
 
 @app.after_startup
-async def test_send(kv: KeyValue):
-    await kv.put("key", b"Hello!")
-    await broker.publish("Hi!", "subject")
+async def setup_broker():
+    key_value = await broker.key_value(bucket="bucket")
+    await key_value.put("key", b"Hello!")
