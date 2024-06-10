c20629c6fe0948f5eb35f13c7afc2b7dfe9584ed
Pastukhov Nikita <nikita@pastukhov-dev.ru>
2024-06-10 17:23:48 +0300

    docs: update filters example (#1516)
    * docs: update filters examples
    
    * chore: update dependencies

--- docs/docs_src/getting_started/subscription/kafka/filter.py
+++ docs/docs_src/getting_started/subscription/kafka/filter.py
@@ -4,19 +4,19 @@ from faststream.kafka import KafkaBroker
 broker = KafkaBroker("localhost:9092")
 app = FastStream(broker)
 
+subscriber = broker.subscriber("test-topic")
 
-@broker.subscriber(
-    "test-topic",
+@subscriber(
     filter=lambda msg: msg.content_type == "application/json",
 )
 async def handle(name: str, user_id: int):
     assert name == "John"
     assert user_id == 1
 
-
-@broker.subscriber("test-topic")
+@subscriber
 async def default_handler(msg: str):
     assert msg == "Hello, FastStream!"
+
 
 
 @app.after_startup
