df2a0a138ad1784a76ba6689a6da1b661c279951
Kumaran Rajendhiran <kumaran@airt.ai>
2024-06-11 10:44:29 +0530

    Add config param to pass additional parameters to confluent-kafka-python (#1505)
    * Add config param to pass additional parameters to confluent-kafka-python
    
    * Raise error if ssl_context is passed
    
    * Add ConfluentConfig typed dict
    
    * Add docs for passing config
    
    * Fix mypy failure
    
    * Remove unused parameters
    
    * Bump version

--- docs/docs_src/confluent/security/sasl_scram256.py
+++ docs/docs_src/confluent/security/sasl_scram256.py
@@ -1,13 +1,10 @@
-import ssl
-
 from faststream.confluent import KafkaBroker
 from faststream.security import SASLScram256
 
-ssl_context = ssl.create_default_context()
 security = SASLScram256(
-    ssl_context=ssl_context,
     username="admin",
     password="password",  # pragma: allowlist secret
+    use_ssl=True,
 )
 
 broker = KafkaBroker("localhost:9092", security=security)
--- faststream/confluent/schemas/params.py
+++ faststream/confluent/schemas/params.py
@@ -1,4 +1,3 @@
-import ssl
 from asyncio import AbstractEventLoop
 from typing import List, Literal, Optional, Union
 
@@ -11,14 +10,12 @@ class ConsumerConnectionParams(TypedDict, total=False):
     bootstrap_servers: Union[str, List[str]]
     loop: Optional[AbstractEventLoop]
     client_id: str
-    request_timeout_ms: int
     retry_backoff_ms: int
     metadata_max_age_ms: int
     security_protocol: Literal[
         "SSL",
         "PLAINTEXT",
     ]
-    api_version: str
     connections_max_idle_ms: int
     sasl_mechanism: Literal[
         "PLAIN",
@@ -29,7 +26,3 @@ class ConsumerConnectionParams(TypedDict, total=False):
     ]
     sasl_plain_password: str
     sasl_plain_username: str
-    sasl_kerberos_service_name: str
-    sasl_kerberos_domain_name: str
-    ssl_context: ssl.SSLContext
-    sasl_oauth_token_provider: str
