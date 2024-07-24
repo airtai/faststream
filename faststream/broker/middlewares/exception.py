45ccb301990085eb17dbafd552e5e2d658f65011
Ruslan Alimov <rusichviz@gmail.com>
2024-07-24 11:21:25 +0300

    feature/exception_middleware refactored exception miffleware

--- faststream/broker/middlewares/exception.py
+++ faststream/broker/middlewares/exception.py
@@ -1,41 +1,67 @@
-from typing import Awaitable, Callable, Optional
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict, Optional
 
 from faststream.broker.middlewares.base import BaseMiddleware
 
+if TYPE_CHECKING:
+    from faststream.broker.message import StreamMessage
+    from faststream.types import AsyncFuncAny
 
-class ExceptionMiddleware(BaseMiddleware):
+
+class BaseExceptionMiddleware(BaseMiddleware):
+
+    def __init__(
+        self,
+        exception_handlers: Dict[
+            Exception, Callable[[Exception], Awaitable[None]]
+        ],
+        msg: Optional[Any] = None,
+    ) -> None:
+        super().__init__(msg)
+        self._exception_handlers = exception_handlers
+
+    async def consume_scope(
+        self,
+        call_next: "AsyncFuncAny",
+        msg: "StreamMessage[Any]",
+    ) -> Any:
+        err: Optional[Exception] = None
+        try:
+            result = await call_next(await self.on_consume(msg))
+
+        except Exception as exc:
+            handler = self._exception_handlers.get(type(exc))
+            if handler:
+                await handler(exc)
+            else:
+                err = exc
+
+        else:
+            return result
+
+        finally:
+            await self.after_consume(err)
+
+
+class ExceptionMiddleware:
+    __slots__ = ("_exception_handlers",)
 
     def __init__(
         self,
         exception_handlers: Optional[
-            dict[Exception, Callable[[Exception], Awaitable[None]]]] = None
+            Dict[Exception, Callable[[Exception], Awaitable[None]]]
+        ] = None
     ) -> None:
-        super().__init__(msg=None)
         if not exception_handlers:
             exception_handlers = {}
-        self.exception_handlers = exception_handlers
-
-    async def after_consume(self, err: Optional[Exception]) -> None:
-        await self._check_handlers(err)
-
-    async def after_publish(self, err: Optional[Exception]) -> None:
-        await self._check_handlers(err)
-
-    async def _check_handlers(self, err: Optional[Exception]):
-        if err:
-            handler = self.exception_handlers.get(type(err))
-            if not handler:
-                raise err
-
-            await handler(err)
+        self._exception_handlers = exception_handlers
 
     def add_handler(self, exc: Exception) -> Callable:
-
         def wrapper(func: Callable) -> None:
-            self.exception_handlers[exc] = func
+            self._exception_handlers[exc] = func
 
         return wrapper
 
-    def __call__(self, msg):
-        self.msg = msg
-        return self
+    def __call__(self, msg: Optional[Any]) -> BaseMiddleware:
+        return BaseExceptionMiddleware(
+            exception_handlers=self._exception_handlers, msg=msg
+        )
