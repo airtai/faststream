ef58a96a84e96209c41db0acf59ce54efd8777d5
Filip Danieluk <filip.danieluk@cbre.com>
2024-07-30 15:50:48 +0200

    added one more test

--- tests/asyncapi/confluent/test_security.py
+++ tests/asyncapi/confluent/test_security.py
@@ -9,6 +9,7 @@ from faststream.security import (
     SASLPlaintext,
     SASLScram256,
     SASLScram512,
+    SASLOAuthBearer,
 )
 
 basic_schema = {
@@ -167,3 +168,28 @@ def test_scram512_security_schema():
     }
 
     assert schema == sasl512_security_schema
+
+
+def test_oauthbearer_security_schema():
+    ssl_context = ssl.create_default_context()
+    security = SASLOAuthBearer(
+        ssl_context=ssl_context,
+    )
+
+    broker = KafkaBroker("localhost:9092", security=security)
+    app = FastStream(broker)
+
+    @broker.publisher("test_2")
+    @broker.subscriber("test_1")
+    async def test_topic(msg: str) -> str:
+        pass
+
+    schema = get_app_schema(app).to_jsonable()
+
+    sasl_oauthbearer_security_schema = deepcopy(basic_schema)
+    sasl_oauthbearer_security_schema["servers"]["development"]["security"] = [{"oauthbearer": []}]
+    sasl_oauthbearer_security_schema["components"]["securitySchemes"] = {
+        "oauthbearer": {"type": "oauthBearer"}
+    }
+
+    assert schema == sasl_oauthbearer_security_schema
