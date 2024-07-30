0aa09f27ef8dcdb1c89edc9085dd4fd5bfb7b221
Ruslan Alimov <rusichviz@gmail.com>
2024-07-30 09:26:14 +0300

    feature/exception_middleware improved type hints, added tests

--- tests/brokers/confluent/test_middlewares.py
+++ tests/brokers/confluent/test_middlewares.py
@@ -3,11 +3,24 @@ from typing import Any, ClassVar, Dict
 import pytest
 
 from faststream.confluent import KafkaBroker
-from tests.brokers.base.middlewares import MiddlewareTestcase
+from tests.brokers.base.middlewares import (
+    ExceptionMiddlewareTestcase,
+    MiddlewareTestcase,
+)
 
 
 @pytest.mark.confluent()
 class TestMiddlewares(MiddlewareTestcase):
     broker_class = KafkaBroker
     timeout: int = 10
-    subscriber_kwargs: ClassVar[Dict[str, Any]] = {"auto_offset_reset": "earliest"}
+    subscriber_kwargs: ClassVar[Dict[str, Any]] = {
+        "auto_offset_reset": "earliest"}
+
+
+@pytest.mark.confluent()
+class TestExceptionMiddlewares(ExceptionMiddlewareTestcase):
+    broker_class = KafkaBroker
+    timeout: int = 10
+    subscriber_kwargs: ClassVar[Dict[str, Any]] = {
+        "auto_offset_reset": "earliest"
+    }
