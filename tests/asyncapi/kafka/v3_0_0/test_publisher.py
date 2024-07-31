bb7d2a81819ed2bcf4c8497d9078c467109c3937
Vladimir Kibisov <kryseyt@gmail.com>
2024-07-31 20:16:33 +0300

    AsyncAPI 3.0.0 Kafka publisher tests

---
+++ tests/asyncapi/kafka/v3_0_0/test_publisher.py
@@ -0,0 +1,20 @@
+from faststream.asyncapi.generate import get_app_schema
+from faststream.kafka import KafkaBroker
+from tests.asyncapi.base.v3_0_0.publisher import PublisherTestcase
+
+
+class TestArguments(PublisherTestcase):
+    broker_class = KafkaBroker
+
+    def test_publisher_bindings(self):
+        broker = self.broker_class()
+
+        @broker.publisher("test")
+        async def handle(msg): ...
+
+        schema = get_app_schema(self.build_app(broker)).to_jsonable()
+        key = tuple(schema["channels"].keys())[0]  # noqa: RUF015
+
+        assert schema["channels"][key]["bindings"] == {
+            "kafka": {"bindingVersion": "0.4.0", "topic": "test"}
+        }
