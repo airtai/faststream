cd35752862078eb336b6a413557eabce53962667
Vladimir Kibisov <kryseyt@gmail.com>
2024-08-01 10:15:18 +0300

    AsyncAPI 3.0.0 Confluent router tests

---
+++ tests/asyncapi/confluent/v3_0_0/test_router.py
@@ -0,0 +1,114 @@
+from faststream import FastStream
+from faststream.asyncapi.generate import get_app_schema
+from faststream.asyncapi.version import AsyncAPIVersion
+from faststream.confluent import KafkaBroker, KafkaPublisher, KafkaRoute, KafkaRouter
+from tests.asyncapi.base.arguments import ArgumentsTestcase
+from tests.asyncapi.base.publisher import PublisherTestcase
+from tests.asyncapi.base.v3_0_0.router import RouterTestcase
+
+
+class TestRouter(RouterTestcase):
+    broker_class = KafkaBroker
+    router_class = KafkaRouter
+    route_class = KafkaRoute
+    publisher_class = KafkaPublisher
+
+    def test_prefix(self):
+        broker = self.broker_class()
+
+        router = self.router_class(prefix="test_")
+
+        @router.subscriber("test")
+        async def handle(msg): ...
+
+        broker.include_router(router)
+
+        schema = get_app_schema(FastStream(broker, asyncapi_version=AsyncAPIVersion.v3_0)).to_jsonable()
+
+        assert schema == {
+            "info": {
+                "title": "FastStream",
+                "version": "0.1.0",
+                "description": ""
+            },
+            "asyncapi": "3.0.0",
+            "defaultContentType": "application/json",
+            "servers": {
+                "development": {
+                    "host": "localhost",
+                    "pathname": "",
+                    "protocol": "kafka",
+                    "protocolVersion": "auto"
+                }
+            },
+            "channels": {
+                "test_test:Handle": {
+                    "address": "test_test:Handle",
+                    "servers": [
+                        {
+                            "$ref": "#/servers/development"
+                        }
+                    ],
+                    "messages": {
+                        "SubscribeMessage": {
+                            "$ref": "#/components/messages/test_test:Handle:SubscribeMessage"
+                        }
+                    },
+                    "bindings": {
+                        "kafka": {
+                            "topic": "test_test",
+                            "bindingVersion": "0.4.0"
+                        }
+                    }
+                }
+            },
+            "operations": {
+                "test_test:HandleSubscribe": {
+                    "action": "receive",
+                    "messages": [
+                        {
+                            "$ref": "#/channels/test_test:Handle/messages/SubscribeMessage"
+                        }
+                    ],
+                    "channel": {
+                        "$ref": "#/channels/test_test:Handle"
+                    }
+                }
+            },
+            "components": {
+                "messages": {
+                    "test_test:Handle:Message": {
+                        "title": "test_test:Handle:Message",
+                        "correlationId": {
+                            "location": "$message.header#/correlation_id"
+                        },
+                        "payload": {
+                            "$ref": "#/components/schemas/Handle:Message:Payload"
+                        }
+                    }
+                },
+                "schemas": {
+                    "Handle:Message:Payload": {
+                        "title": "Handle:Message:Payload"
+                    }
+                }
+            }
+        }
+
+
+class TestRouterArguments(ArgumentsTestcase):
+    broker_class = KafkaRouter
+
+    def build_app(self, router):
+        broker = KafkaBroker()
+        broker.include_router(router)
+        return FastStream(broker)
+
+
+class TestRouterPublisher(PublisherTestcase):
+    broker_class = KafkaRouter
+
+    def build_app(self, router):
+        broker = KafkaBroker()
+        broker.include_router(router)
+        return FastStream(broker)
