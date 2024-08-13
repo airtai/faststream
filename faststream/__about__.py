4bf70ad38ed1d4d2420365d2a09414b0dbf1dbde
Pastukhov Nikita <nikita@pastukhov-dev.ru>
2024-08-13 16:00:48 +0300

    fix: catch parser errors (#1680)
    * fix: catch Parser & Decoder errors in middlewares
    
    * chore: bump version
    
    * lint: fix codespell

--- faststream/__about__.py
+++ faststream/__about__.py
@@ -1,5 +1,5 @@
 """Simple and fast framework to create message brokers based microservices."""
 
-__version__ = "0.5.17"
+__version__ = "0.5.18"
 
 SERVICE_NAME = f"faststream-{__version__}"
--- faststream/broker/subscriber/usecase.py
+++ faststream/broker/subscriber/usecase.py
@@ -334,9 +334,17 @@ class SubscriberUsecase(
                 await middleware.__aenter__()
 
             cache: Dict[Any, Any] = {}
+            parsing_error: Optional[Exception] = None
             for h in self.calls:
-                if (message := await h.is_suitable(msg, cache)) is not None:
+                try:
+                    message = await h.is_suitable(msg, cache)
+                except Exception as e:
+                    parsing_error = e
+                    break
+
+                if message is not None:
                     # Acknowledgement scope
+                    # TODO: move it to scope enter at `retry` option deprecation
                     await stack.enter_async_context(
                         self.watcher(
                             message,
@@ -377,11 +385,16 @@ class SubscriberUsecase(
 
                     return result_msg.body
 
-            # Suitable handler is not founded
+            # Suitable handler was not found or
+            # parsing/decoding exception occurred
             for m in middlewares:
                 stack.push_async_exit(m.__aexit__)
 
-            raise AssertionError(f"There is no suitable handler for {msg=}")
+            if parsing_error:
+                raise parsing_error
+
+            else:
+                raise AssertionError(f"There is no suitable handler for {msg=}")
 
         return None
 
