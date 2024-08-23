9961017d4c8baa53fa3b7c32abfabb5d5df4079f
Kumaran Rajendhiran <kumaran@airt.ai>
2024-08-23 18:24:30 +0530

    Fix multiple docs issues (#1718)
    * Use mkdocs hooks to show correct edit url for public_api pages
    
    * Use correct workflow path
    
    * Change twitter.com to x.com
    
    * Update .yml to .yaml to fix broken badge
    
    * Remove broken reference to faststream codegen
    
    * Fix formatting of EmailStr API docs link
    
    * Update max response body size

--- docs/create_api_docs.py
+++ docs/create_api_docs.py
@@ -310,7 +310,7 @@ def _generate_api_docs_for_module(root_path: Path, module_name: str) -> Tuple[st
     src = """                    - [ContactDict](api/faststream/asyncapi/schema/info/ContactDict.md)
 """
     dst = """                    - [ContactDict](api/faststream/asyncapi/schema/info/ContactDict.md)
-                    - [EmailStr](api/faststream/asyncapi/schema/info/EmailStr.md)
+                        - [EmailStr](api/faststream/asyncapi/schema/info/EmailStr.md)
 """
     api_summary = api_summary.replace(src, dst)
 
@@ -340,6 +340,12 @@ def create_api_docs(
     summary = "\n".join(filter(bool, (x.rstrip() for x in summary.split("\n"))))
 
     (docs_dir / "SUMMARY.md").write_text(summary)
+
+
+def on_page_markdown(markdown, *, page, config, files):
+    """Mkdocs hook to update the edit URL for the public API pages."""
+    if "public_api" in page.edit_url:
+        page.edit_url = page.edit_url.replace("public_api", "api")
 
 
 if __name__ == "__main__":
