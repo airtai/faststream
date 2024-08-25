8ed62f8abdb071ec9827a0a8331683fe5bcfde60
Nikita Pastukhov <diementros@yandex.ru>
2024-08-25 14:34:32 +0300

    refactor: change publisher fake subscriber generation logic

--- faststream/testing/broker.py
+++ faststream/testing/broker.py
@@ -8,6 +8,7 @@ from typing import (
     AsyncGenerator,
     Generator,
     Generic,
+    List,
     Optional,
     Tuple,
     Type,
@@ -17,8 +18,6 @@ from unittest import mock
 from unittest.mock import MagicMock
 
 from faststream.broker.core.usecase import BrokerUsecase
-from faststream.broker.middlewares.logging import CriticalLogMiddleware
-from faststream.broker.wrapper.call import HandlerCallWrapper
 from faststream.testing.app import TestApp
 from faststream.utils.ast import is_contains_context_name
 from faststream.utils.functions import sync_fake_context
@@ -26,7 +25,8 @@ from faststream.utils.functions import sync_fake_context
 if TYPE_CHECKING:
     from types import TracebackType
 
-    from faststream.broker.types import BrokerMiddleware
+    from faststream.broker.subscriber.proto import SubscriberProto
+
 
 Broker = TypeVar("Broker", bound=BrokerUsecase[Any, Any])
 
@@ -65,6 +65,7 @@ class TestBroker(Generic[Broker]):
                 connect_only = False
 
         self.connect_only = connect_only
+        self._fake_subscribers: List[SubscriberProto[Any]] = []
 
     async def __aenter__(self) -> Broker:
         self._ctx = self._create_ctx()
@@ -72,23 +73,6 @@ class TestBroker(Generic[Broker]):
 
     async def __aexit__(self, *args: Any) -> None:
         await self._ctx.__aexit__(*args)
-
-        # TODO: remove useless middlewares filter
-        middlewares: Tuple[BrokerMiddleware[Any], ...] = (
-            CriticalLogMiddleware(
-                logger=self.broker.logger,
-                log_level=self.broker._msg_log_level,
-            ),
-            *self.broker._middlewares,
-        )
-
-        self.broker._middlewares = middlewares
-
-        for sub in self.broker._subscribers.values():
-            sub._broker_middlewares = middlewares
-
-        for pub in self.broker._publishers.values():
-            pub._broker_middlewares = middlewares
 
     @asynccontextmanager
     async def _create_ctx(self) -> AsyncGenerator[Broker, None]:
@@ -107,17 +91,16 @@ class TestBroker(Generic[Broker]):
                 finally:
                     self._fake_close(self.broker)
 
-    @classmethod
     @contextmanager
-    def _patch_broker(cls, broker: Broker) -> Generator[None, None, None]:
+    def _patch_broker(self, broker: Broker) -> Generator[None, None, None]:
         with mock.patch.object(
             broker,
             "start",
-            wraps=partial(cls._fake_start, broker),
+            wraps=partial(self._fake_start, broker),
         ), mock.patch.object(
             broker,
             "_connect",
-            wraps=partial(cls._fake_connect, broker),
+            wraps=partial(self._fake_connect, broker),
         ), mock.patch.object(
             broker,
             "close",
@@ -136,38 +119,47 @@ class TestBroker(Generic[Broker]):
         ):
             yield
 
-    @classmethod
-    def _fake_start(cls, broker: Broker, *args: Any, **kwargs: Any) -> None:
+    def _fake_start(self, broker: Broker, *args: Any, **kwargs: Any) -> None:
         broker.setup()
 
         patch_broker_calls(broker)
 
-        for key, p in broker._publishers.items():
+        for p in broker._publishers.values():
             if getattr(p, "_fake_handler", None):
                 continue
 
-            handler = broker._subscribers.get(key)
+            sub, is_real = self.create_publisher_fake_subscriber(broker, p)
 
-            if handler is not None:
+            if not is_real:
+                self._fake_subscribers.append(sub)
+
+            if not sub.calls:
+
+                @sub
+                async def publisher_response_subscriber(msg: Any) -> None:
+                    pass
+
+                broker.setup_subscriber(sub)
+
+            if is_real:
                 mock = MagicMock()
                 p.set_test(mock=mock, with_fake=False)  # type: ignore[attr-defined]
-                for h in handler.calls:
+                for h in sub.calls:
                     h.handler.set_test()
                     assert h.handler.mock  # nosec B101
                     h.handler.mock.side_effect = mock
 
             else:
-                f = cls.create_publisher_fake_subscriber(broker, p)
-                f.set_test()
-                assert f.mock  # nosec B101
-                p.set_test(mock=f.mock, with_fake=True)  # type: ignore[attr-defined]
+                handler = sub.calls[0].handler
+                handler.set_test()
+                assert handler.mock  # nosec B101
+                p.set_test(mock=handler.mock, with_fake=True)  # type: ignore[attr-defined]
 
-        for handler in broker._subscribers.values():
-            handler.running = True
+        for subscriber in broker._subscribers.values():
+            subscriber.running = True
 
-    @classmethod
     def _fake_close(
-        cls,
+        self,
         broker: Broker,
         exc_type: Optional[Type[BaseException]] = None,
         exc_val: Optional[BaseException] = None,
@@ -176,7 +168,10 @@ class TestBroker(Generic[Broker]):
         for p in broker._publishers.values():
             if getattr(p, "_fake_handler", None):
                 p.reset_test()  # type: ignore[attr-defined]
-                cls.remove_publisher_fake_subscriber(broker, p)
+
+        for sub in self._fake_subscribers:
+            self.broker._subscribers.pop(hash(sub), None)  # type: ignore[attr-defined]
+        self._fake_subscribers = []
 
         for h in broker._subscribers.values():
             h.running = False
@@ -187,18 +182,13 @@ class TestBroker(Generic[Broker]):
     @abstractmethod
     def create_publisher_fake_subscriber(
         broker: Broker, publisher: Any
-    ) -> HandlerCallWrapper[Any, Any, Any]:
-        raise NotImplementedError()
-
-    @staticmethod
-    @abstractmethod
-    def remove_publisher_fake_subscriber(broker: Broker, publisher: Any) -> None:
-        raise NotImplementedError()
+    ) -> Tuple["SubscriberProto[Any]", bool]:
+        raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     async def _fake_connect(broker: Broker, *args: Any, **kwargs: Any) -> None:
-        raise NotImplementedError()
+        raise NotImplementedError
 
 
 def patch_broker_calls(broker: "BrokerUsecase[Any, Any]") -> None:
