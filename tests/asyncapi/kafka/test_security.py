86db950b724ad0a92688a82312b599d5f29a805b
Alex Sinnott <alexander.sinnott@transferwise.com>
2024-09-05 09:56:10 +0100

    Added same OAUTHBEARER security parsing as Confluent Kafka to AIO Kafka

--- tests/asyncapi/kafka/test_security.py
+++ tests/asyncapi/kafka/test_security.py
@@ -9,7 +9,7 @@ from faststream.security import (
     BaseSecurity,
     SASLPlaintext,
     SASLScram256,
-    SASLScram512,
+    SASLScram512, SASLOAuthBearer,
 )
 
 basic_schema = {
@@ -168,6 +168,33 @@ def test_scram512_security_schema():
     }
 
     assert schema == sasl512_security_schema
+
+
+def test_oauthbearer_security_schema():
+    ssl_context = ssl.create_default_context()
+    security = SASLOAuthBearer(
+        ssl_context=ssl_context,
+    )
+
+    broker = KafkaBroker("localhost:9092", security=security)
+    app = FastStream(broker)
+
+    @broker.publisher("test_2")
+    @broker.subscriber("test_1")
+    async def test_topic(msg: str) -> str:
+        pass
+
+    schema = get_app_schema(app).to_jsonable()
+
+    sasl_oauthbearer_security_schema = deepcopy(basic_schema)
+    sasl_oauthbearer_security_schema["servers"]["development"]["security"] = [
+        {"oauthbearer": []}
+    ]
+    sasl_oauthbearer_security_schema["components"]["securitySchemes"] = {
+        "oauthbearer": {"type": "oauthBearer"}
+    }
+
+    assert schema == sasl_oauthbearer_security_schema
 
 
 def test_gssapi_security_schema():
