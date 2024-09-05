60ceaa99db2f7defd15bf3428ed309d85e79b6ae
Alex Sinnott <alexander.sinnott@transferwise.com>
2024-09-05 09:53:36 +0100

    Added same OAUTHBEARER security parsing as Confluent Kafka to AIO Kafka

--- faststream/kafka/security.py
+++ faststream/kafka/security.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Optional
 from faststream.security import (
     SASLGSSAPI,
     BaseSecurity,
+    SASLOAuthBearer,
     SASLPlaintext,
     SASLScram256,
     SASLScram512,
@@ -21,6 +22,8 @@ def parse_security(security: Optional[BaseSecurity]) -> "AnyDict":
         return _parse_sasl_scram256(security)
     elif isinstance(security, SASLScram512):
         return _parse_sasl_scram512(security)
+    elif isinstance(security, SASLOAuthBearer):
+        return _parse_sasl_oauthbearer(security)
     elif isinstance(security, SASLGSSAPI):
         return _parse_sasl_gssapi(security)
     elif isinstance(security, BaseSecurity):
@@ -63,6 +66,13 @@ def _parse_sasl_scram512(security: SASLScram512) -> "AnyDict":
         "sasl_mechanism": "SCRAM-SHA-512",
         "sasl_plain_username": security.username,
         "sasl_plain_password": security.password,
+    }
+
+
+def _parse_sasl_oauthbearer(security: SASLOAuthBearer) -> "AnyDict":
+    return {
+        "security_protocol": "SASL_SSL" if security.use_ssl else "SASL_PLAINTEXT",
+        "sasl_mechanism": "OAUTHBEARER",
     }
 
 
