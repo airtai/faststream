27a910d52635aa40a273df04c691aee105eed2f0
Nikita Pastukhov <diementros@yandex.ru>
2024-09-25 19:11:35 +0300

    refactor: reuse HAS_TYPER in CLI

--- faststream/__main__.py
+++ faststream/__main__.py
@@ -2,20 +2,17 @@
 
 import warnings
 
-try:
-    from faststream._internal.cli.main import cli
-except ImportError:
-    has_typer = False
-else:
-    has_typer = True
+from faststream._internal._compat import HAS_TYPER
 
-if not has_typer:
+if not HAS_TYPER:
     raise ImportError(
         "\n\nYou're trying to use the FastStream CLI, "
         "\nbut you haven't installed the required dependencies."
         "\nPlease install them using the following command: "
         '\npip install "faststream[cli]"'
     )
+
+from faststream._internal.cli.main import cli
 
 warnings.filterwarnings("default", category=ImportWarning, module="faststream")
 
