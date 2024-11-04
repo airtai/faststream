e6e1d0a9b4d393d07e7e32a1715fddacac2803e4
Roma Frolov <f.roman34@mail.ru>
2024-11-04 22:20:43 +0300

    tests on MetricsSettingsProvider for all brokers

---
+++ tests/prometheus/kafka/test_provider.py
@@ -0,0 +1,101 @@
+import random
+from types import SimpleNamespace
+
+import pytest
+
+from faststream.kafka.prometheus.provider import (
+    BatchKafkaMetricsSettingsProvider,
+    KafkaMetricsSettingsProvider,
+    settings_provider_factory,
+)
+from faststream.prometheus import MetricsSettingsProvider
+from tests.prometheus.basic import LocalMetricsSettingsProviderTestcase
+
+
+class LocalBaseKafkaMetricsSettingsProviderTestcase(
+    LocalMetricsSettingsProviderTestcase
+):
+    messaging_system = "kafka"
+
+    def test_get_publish_destination_name_from_cmd(self, queue: str) -> None:
+        expected_destination_name = queue
+        provider = self.get_provider()
+        command = SimpleNamespace(destination=queue)
+
+        destination_name = provider.get_publish_destination_name_from_cmd(command)
+
+        assert destination_name == expected_destination_name
+
+
+class TestKafkaMetricsSettingsProvider(LocalBaseKafkaMetricsSettingsProviderTestcase):
+    @staticmethod
+    def get_provider() -> MetricsSettingsProvider:
+        return KafkaMetricsSettingsProvider()
+
+    def test_get_consume_attrs_from_message(self, queue: str) -> None:
+        body = b"Hello"
+        expected_attrs = {
+            "destination_name": queue,
+            "message_size": len(body),
+            "messages_count": 1,
+        }
+
+        message = SimpleNamespace(body=body, raw_message=SimpleNamespace(topic=queue))
+
+        provider = self.get_provider()
+        attrs = provider.get_consume_attrs_from_message(message)
+
+        assert attrs == expected_attrs
+
+
+class TestBatchKafkaMetricsSettingsProvider(
+    LocalBaseKafkaMetricsSettingsProviderTestcase
+):
+    @staticmethod
+    def get_provider() -> MetricsSettingsProvider:
+        return BatchKafkaMetricsSettingsProvider()
+
+    def test_get_consume_attrs_from_message(self, queue: str) -> None:
+        body = [b"Hi ", b"again, ", b"FastStream!"]
+        message = SimpleNamespace(
+            body=body,
+            raw_message=[
+                SimpleNamespace(topic=queue) for _ in range(random.randint(a=2, b=10))
+            ],
+        )
+        expected_attrs = {
+            "destination_name": message.raw_message[0].topic,
+            "message_size": len(bytearray().join(body)),
+            "messages_count": len(message.raw_message),
+        }
+
+        provider = self.get_provider()
+        attrs = provider.get_consume_attrs_from_message(message)
+
+        assert attrs == expected_attrs
+
+
+@pytest.mark.parametrize(
+    ("msg", "expected_provider"),
+    (
+        pytest.param(
+            (SimpleNamespace(), SimpleNamespace()),
+            BatchKafkaMetricsSettingsProvider(),
+            id="message is batch",
+        ),
+        pytest.param(
+            SimpleNamespace(),
+            KafkaMetricsSettingsProvider(),
+            id="single message",
+        ),
+        pytest.param(
+            None,
+            KafkaMetricsSettingsProvider(),
+            id="message is None",
+        ),
+    ),
+)
+def test_settings_provider_factory(msg, expected_provider) -> None:
+    provider = settings_provider_factory(msg)
+
+    assert isinstance(provider, type(expected_provider))
---
+++ tests/prometheus/redis/test_provider.py
@@ -0,0 +1,125 @@
+from types import SimpleNamespace
+
+import pytest
+
+from faststream.prometheus import MetricsSettingsProvider
+from faststream.redis.prometheus.provider import (
+    BatchRedisMetricsSettingsProvider,
+    RedisMetricsSettingsProvider,
+    settings_provider_factory,
+)
+from tests.prometheus.basic import LocalMetricsSettingsProviderTestcase
+
+
+class LocalBaseRedisMetricsSettingsProviderTestcase(
+    LocalMetricsSettingsProviderTestcase
+):
+    messaging_system = "redis"
+
+    def test_get_publish_destination_name_from_cmd(self, queue: str) -> None:
+        expected_destination_name = queue
+        provider = self.get_provider()
+        command = SimpleNamespace(destination=queue)
+
+        destination_name = provider.get_publish_destination_name_from_cmd(command)
+
+        assert destination_name == expected_destination_name
+
+
+class TestRedisMetricsSettingsProvider(LocalBaseRedisMetricsSettingsProviderTestcase):
+    @staticmethod
+    def get_provider() -> MetricsSettingsProvider:
+        return RedisMetricsSettingsProvider()
+
+    @pytest.mark.parametrize(
+        "destination",
+        (
+            pytest.param("channel", id="destination is channel"),
+            pytest.param("list", id="destination is list"),
+            pytest.param("stream", id="destination is stream"),
+            pytest.param("", id="destination is blank"),
+        ),
+    )
+    def test_get_consume_attrs_from_message(self, queue: str, destination: str) -> None:
+        body = b"Hello"
+        expected_attrs = {
+            "destination_name": queue if destination else "",
+            "message_size": len(body),
+            "messages_count": 1,
+        }
+        raw_message = {}
+
+        if destination:
+            raw_message[destination] = queue
+
+        message = SimpleNamespace(body=body, raw_message=raw_message)
+
+        provider = self.get_provider()
+        attrs = provider.get_consume_attrs_from_message(message)
+
+        assert attrs == expected_attrs
+
+
+class TestBatchRedisMetricsSettingsProvider(
+    LocalBaseRedisMetricsSettingsProviderTestcase
+):
+    @staticmethod
+    def get_provider() -> MetricsSettingsProvider:
+        return BatchRedisMetricsSettingsProvider()
+
+    @pytest.mark.parametrize(
+        "destination",
+        (
+            pytest.param("channel", id="destination is channel"),
+            pytest.param("list", id="destination is list"),
+            pytest.param("stream", id="destination is stream"),
+            pytest.param("", id="destination is blank"),
+        ),
+    )
+    def test_get_consume_attrs_from_message(self, queue: str, destination: str) -> None:
+        decoded_body = ["Hi ", "again, ", "FastStream!"]
+        body = str(decoded_body).encode()
+        expected_attrs = {
+            "destination_name": queue if destination else "",
+            "message_size": len(body),
+            "messages_count": len(decoded_body),
+        }
+        raw_message = {}
+
+        if destination:
+            raw_message[destination] = queue
+
+        message = SimpleNamespace(
+            body=body, _decoded_body=decoded_body, raw_message=raw_message
+        )
+
+        provider = self.get_provider()
+        attrs = provider.get_consume_attrs_from_message(message)
+
+        assert attrs == expected_attrs
+
+
+@pytest.mark.parametrize(
+    ("msg", "expected_provider"),
+    (
+        pytest.param(
+            {"type": "blist"},
+            BatchRedisMetricsSettingsProvider(),
+            id="message is batch",
+        ),
+        pytest.param(
+            {"type": "not_blist"},
+            RedisMetricsSettingsProvider(),
+            id="single message",
+        ),
+        pytest.param(
+            None,
+            RedisMetricsSettingsProvider(),
+            id="message is None",
+        ),
+    ),
+)
+def test_settings_provider_factory(msg, expected_provider) -> None:
+    provider = settings_provider_factory(msg)
+
+    assert isinstance(provider, type(expected_provider))
