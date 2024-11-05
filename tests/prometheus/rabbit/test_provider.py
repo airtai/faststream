0fa5fc1fe2ab873902df55ab6354a6e1a9990760
Roma Frolov <98967567+roma-frolov@users.noreply.github.com>
2024-11-05 15:56:12 +0300

    tests on MetricsSettingsProvider for all brokers (#1890)
    * tests on MetricsSettingsProvider for all brokers
    
    * chore: fix tests
    
    * chore: remove loguru usage
    
    ---------
    
    Co-authored-by: Nikita Pastukhov <diementros@yandex.ru>

---
+++ tests/prometheus/rabbit/test_provider.py
@@ -0,0 +1,65 @@
+from types import SimpleNamespace
+from typing import Union
+
+import pytest
+
+from faststream.prometheus import MetricsSettingsProvider
+from faststream.rabbit.prometheus.provider import RabbitMetricsSettingsProvider
+from tests.prometheus.basic import LocalMetricsSettingsProviderTestcase
+
+
+class TestRabbitMetricsSettingsProvider(LocalMetricsSettingsProviderTestcase):
+    messaging_system = "rabbitmq"
+
+    @staticmethod
+    def get_provider() -> MetricsSettingsProvider:
+        return RabbitMetricsSettingsProvider()
+
+    @pytest.mark.parametrize(
+        "exchange",
+        (
+            pytest.param("my_exchange", id="with exchange"),
+            pytest.param(None, id="without exchange"),
+        ),
+    )
+    def test_get_consume_attrs_from_message(
+        self,
+        exchange: Union[str, None],
+        queue: str,
+    ) -> None:
+        body = b"Hello"
+        expected_attrs = {
+            "destination_name": f"{exchange or 'default'}.{queue}",
+            "message_size": len(body),
+            "messages_count": 1,
+        }
+        message = SimpleNamespace(
+            body=body, raw_message=SimpleNamespace(exchange=exchange, routing_key=queue)
+        )
+
+        provider = self.get_provider()
+        attrs = provider.get_consume_attrs_from_message(message)
+
+        assert attrs == expected_attrs
+
+    @pytest.mark.parametrize(
+        "exchange",
+        (
+            pytest.param("my_exchange", id="with exchange"),
+            pytest.param(None, id="without exchange"),
+        ),
+    )
+    def test_get_publish_destination_name_from_cmd(
+        self,
+        exchange: Union[str, None],
+        queue: str,
+    ) -> None:
+        expected_destination_name = f"{exchange or 'default'}.{queue}"
+        command = SimpleNamespace(
+            exchange=SimpleNamespace(name=exchange), destination=queue
+        )
+
+        provider = self.get_provider()
+        destination_name = provider.get_publish_destination_name_from_cmd(command)
+
+        assert destination_name == expected_destination_name
