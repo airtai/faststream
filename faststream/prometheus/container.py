53e435ec3f764177d3034070ec44311b0f466f24
Roma Frolov <98967567+roma-frolov@users.noreply.github.com>
2024-11-19 17:23:47 +0300

    fixed a bug when using one register for several middleware (#1921)
    * fixed a bug when using one register for several middleware
    
    * changed the way of metrics registration
    
    * fixed types
    
    * spaces for easy reading

--- faststream/prometheus/container.py
+++ faststream/prometheus/container.py
@@ -1,6 +1,10 @@
-from typing import Optional, Sequence
+from typing import TYPE_CHECKING, Optional, Sequence, Union, cast
 
-from prometheus_client import CollectorRegistry, Counter, Gauge, Histogram
+from prometheus_client import Counter, Gauge, Histogram
+
+if TYPE_CHECKING:
+    from prometheus_client import CollectorRegistry
+    from prometheus_client.registry import Collector
 
 
 class MetricsContainer:
@@ -43,58 +47,110 @@ class MetricsContainer:
         self._registry = registry
         self._metrics_prefix = metrics_prefix
 
-        self.received_messages_total = Counter(
+        self.received_messages_total = cast(
+            Counter,
+            self._get_registered_metric(f"{metrics_prefix}_received_messages_total"),
+        ) or Counter(
             name=f"{metrics_prefix}_received_messages_total",
             documentation="Count of received messages by broker and handler",
             labelnames=["app_name", "broker", "handler"],
             registry=registry,
         )
-        self.received_messages_size_bytes = Histogram(
+
+        self.received_messages_size_bytes = cast(
+            Histogram,
+            self._get_registered_metric(
+                f"{metrics_prefix}_received_messages_size_bytes"
+            ),
+        ) or Histogram(
             name=f"{metrics_prefix}_received_messages_size_bytes",
             documentation="Histogram of received messages size in bytes by broker and handler",
             labelnames=["app_name", "broker", "handler"],
             registry=registry,
             buckets=received_messages_size_buckets or self.DEFAULT_SIZE_BUCKETS,
         )
-        self.received_messages_in_process = Gauge(
+
+        self.received_messages_in_process = cast(
+            Gauge,
+            self._get_registered_metric(
+                f"{metrics_prefix}_received_messages_in_process"
+            ),
+        ) or Gauge(
             name=f"{metrics_prefix}_received_messages_in_process",
             documentation="Gauge of received messages in process by broker and handler",
             labelnames=["app_name", "broker", "handler"],
             registry=registry,
         )
-        self.received_processed_messages_total = Counter(
+
+        self.received_processed_messages_total = cast(
+            Counter,
+            self._get_registered_metric(
+                f"{metrics_prefix}_received_processed_messages_total"
+            ),
+        ) or Counter(
             name=f"{metrics_prefix}_received_processed_messages_total",
             documentation="Count of received processed messages by broker, handler and status",
             labelnames=["app_name", "broker", "handler", "status"],
             registry=registry,
         )
-        self.received_processed_messages_duration_seconds = Histogram(
+
+        self.received_processed_messages_duration_seconds = cast(
+            Histogram,
+            self._get_registered_metric(
+                f"{metrics_prefix}_received_processed_messages_duration_seconds"
+            ),
+        ) or Histogram(
             name=f"{metrics_prefix}_received_processed_messages_duration_seconds",
             documentation="Histogram of received processed messages duration in seconds by broker and handler",
             labelnames=["app_name", "broker", "handler"],
             registry=registry,
         )
-        self.received_processed_messages_exceptions_total = Counter(
+
+        self.received_processed_messages_exceptions_total = cast(
+            Counter,
+            self._get_registered_metric(
+                f"{metrics_prefix}_received_processed_messages_exceptions_total"
+            ),
+        ) or Counter(
             name=f"{metrics_prefix}_received_processed_messages_exceptions_total",
             documentation="Count of received processed messages exceptions by broker, handler and exception_type",
             labelnames=["app_name", "broker", "handler", "exception_type"],
             registry=registry,
         )
-        self.published_messages_total = Counter(
+
+        self.published_messages_total = cast(
+            Counter,
+            self._get_registered_metric(f"{metrics_prefix}_published_messages_total"),
+        ) or Counter(
             name=f"{metrics_prefix}_published_messages_total",
             documentation="Count of published messages by destination and status",
             labelnames=["app_name", "broker", "destination", "status"],
             registry=registry,
         )
-        self.published_messages_duration_seconds = Histogram(
+
+        self.published_messages_duration_seconds = cast(
+            Histogram,
+            self._get_registered_metric(
+                f"{metrics_prefix}_published_messages_duration_seconds"
+            ),
+        ) or Histogram(
             name=f"{metrics_prefix}_published_messages_duration_seconds",
             documentation="Histogram of published messages duration in seconds by broker and destination",
             labelnames=["app_name", "broker", "destination"],
             registry=registry,
         )
-        self.published_messages_exceptions_total = Counter(
+
+        self.published_messages_exceptions_total = cast(
+            Counter,
+            self._get_registered_metric(
+                f"{metrics_prefix}_published_messages_exceptions_total"
+            ),
+        ) or Counter(
             name=f"{metrics_prefix}_published_messages_exceptions_total",
             documentation="Count of published messages exceptions by broker, destination and exception_type",
             labelnames=["app_name", "broker", "destination", "exception_type"],
             registry=registry,
         )
+
+    def _get_registered_metric(self, metric_name: str) -> Union["Collector", None]:
+        return self._registry._names_to_collectors.get(metric_name)
