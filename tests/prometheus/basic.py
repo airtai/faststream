f751238a109f21d459b5fbeae1957114e6c0d888
Roma Frolov <98967567+roma-frolov@users.noreply.github.com>
2024-11-20 22:55:50 +0300

    fix: HandlerException ingored (#1928)
    * fix: HandlerException ingored
    
    * fix: HandlerException -> IgnoredException ignoring
    
    * fix: ruff

--- tests/prometheus/basic.py
+++ tests/prometheus/basic.py
@@ -7,7 +7,7 @@ from prometheus_client import CollectorRegistry
 
 from faststream import Context
 from faststream.broker.message import AckStatus
-from faststream.exceptions import RejectMessage
+from faststream.exceptions import IgnoredException, RejectMessage
 from faststream.prometheus.middleware import (
     PROCESSING_STATUS_BY_ACK_STATUS,
     PROCESSING_STATUS_BY_HANDLER_EXCEPTION_MAP,
@@ -53,6 +53,11 @@ class LocalPrometheusTestcase(BaseTestcaseConfig):
             pytest.param(AckStatus.nacked, None, id="nacked status without exception"),
             pytest.param(
                 AckStatus.rejected, None, id="rejected status without exception"
+            ),
+            pytest.param(
+                AckStatus.acked,
+                IgnoredException,
+                id="acked status with ignore exception",
             ),
         ],
     )
@@ -176,7 +181,7 @@ class LocalPrometheusTestcase(BaseTestcaseConfig):
             ),
         ]
 
-        if status == ProcessingStatus.error:
+        if exception_class and not issubclass(exception_class, IgnoredException):
             assert (
                 metrics_manager.add_received_processed_message_exception.mock_calls
                 == [
@@ -186,6 +191,11 @@ class LocalPrometheusTestcase(BaseTestcaseConfig):
                         exception_type=exception_class.__name__,
                     ),
                 ]
+            )
+        else:
+            assert (
+                metrics_manager.add_received_processed_message_exception.mock_calls
+                == []
             )
 
     def assert_publish_metrics(self, metrics_manager: Any):
