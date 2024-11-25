a67768c79655a6d4af699f9ac210d8c2c4379643
sheldy <chikibamtioma@gmail.com>
2024-11-25 15:49:10 +0100

    Fix tests again

--- tests/brokers/base/middlewares.py
+++ tests/brokers/base/middlewares.py
@@ -285,7 +285,7 @@ class MiddlewaresOrderTestcase(BaseTestcaseConfig):
         mock.exit_inner.assert_called_once()
         mock.exit_outer.assert_called_once()
 
-        assert [c.args[0] for c in mock.enter.call_args_list] == ["inner", "outer"]
+        assert [c.args[0] for c in mock.enter.call_args_list] == ["outer", "inner"]
         assert [c.args[0] for c in mock.exit.call_args_list] == ["inner", "outer"]
 
 
