183f9fe2aba3353237cd976671a3c781ffc1c2c2
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-26 15:52:27 +0530

    Double the changes, double the fun

--- faststream/dummy.py
+++ faststream/dummy.py
@@ -14,9 +14,27 @@ class Strange:
         return "strange"
 
 
+
+
+
 class Twice:
     def __init__(self) -> None:
         pass
 
     def twice_method(self) -> str:
         return "twice"
+
+class Double:
+    def __init__(self) -> None:
+        pass
+
+    def double_method(self) -> str:
+        return "double"
+    
+
+
+
+
+
+
+    
