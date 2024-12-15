66e9df079eaacdb9386065b6f5ce4c48a34574d2
Pastukhov Nikita <nikita@pastukhov-dev.ru>
2024-12-15 18:37:12 +0300

    Update test_url_builder.py

--- tests/brokers/rabbit/test_url_builder.py
+++ tests/brokers/rabbit/test_url_builder.py
@@ -6,34 +6,33 @@ from yarl import URL
 from faststream.rabbit.utils import build_url
 
 
-class TestUrlBuilder:
-    @pytest.mark.parametrize(
-        ("url_kwargs", "expected_url"),
-        [
-            pytest.param(
-                {},
-                URL("amqp://guest:guest@localhost:5672/"),  # pragma: allowlist secret
-                id="blank_params_use_defaults",
+@pytest.mark.parametrize(
+    ("url_kwargs", "expected_url"),
+    [
+        pytest.param(
+            {},
+            URL("amqp://guest:guest@localhost:5672/"),  # pragma: allowlist secret
+            id="blank params use defaults",
+        ),
+        pytest.param(
+            {"ssl": True},
+            URL("amqps://guest:guest@localhost:5672/"),  # pragma: allowlist secret
+            id="ssl affects protocol",
+        ),
+        pytest.param(
+            {"url": "fake", "virtualhost": "/", "host": "host"},
+            URL("amqp://guest:guest@host:5672/"),  # pragma: allowlist secret
+            id="kwargs overrides url",
+        ),
+        pytest.param(
+            {"virtualhost": "//test"},  # pragma: allowlist secret
+            URL(
+                "amqp://guest:guest@localhost:5672//test"  # pragma: allowlist secret
             ),
-            pytest.param(
-                {"ssl": True},
-                URL("amqps://guest:guest@localhost:5672/"),  # pragma: allowlist secret
-                id="use_ssl",
-            ),
-            pytest.param(
-                {"url": "fake", "virtualhost": "/", "host": "host"},
-                URL("amqp://guest:guest@host:5672/"),  # pragma: allowlist secret
-                id="overrides_and_default_vh",
-            ),
-            pytest.param(
-                {"virtualhost": "//test"},  # pragma: allowlist secret
-                URL(
-                    "amqp://guest:guest@localhost:5672//test"  # pragma: allowlist secret
-                ),
-                id="exotic_virtualhost",
-            ),
-        ],
-    )
-    def test_unpack_args(self, url_kwargs: Dict[str, Any], expected_url: URL) -> None:
-        url = build_url(**url_kwargs)
-        assert url == expected_url
+            id="exotic virtualhost",
+        ),
+    ],
+)
+def test_unpack_args(self, url_kwargs: Dict[str, Any], expected_url: URL) -> None:
+    url = build_url(**url_kwargs)
+    assert url == expected_url
