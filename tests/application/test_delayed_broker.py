155767449b896423ba425a0b3f5247db6a8744fc
Roma Frolov <f.roman34@mail.ru>
2024-12-20 11:08:50 +0300

    feat: addition test

--- tests/application/test_delayed_broker.py
+++ tests/application/test_delayed_broker.py
@@ -1,6 +1,7 @@
 import pytest
 
 from faststream._internal.application import StartAbleApplication
+from faststream.exceptions import SetupError
 from faststream.rabbit import RabbitBroker
 
 
@@ -15,9 +16,24 @@ def test_set_broker() -> None:
     assert app.broker is broker
 
 
+def test_set_more_than_once_broker() -> None:
+    app = StartAbleApplication()
+    broker_1 = RabbitBroker()
+    broker_2 = RabbitBroker()
+
+    app.set_broker(broker_1)
+
+    with pytest.raises(
+        SetupError,
+        match=f"`{app}` already has a broker. You can't use multiple brokers until 1.0.0 release.",
+    ):
+        app.set_broker(broker_2)
+
+
 @pytest.mark.asyncio()
 async def test_start_not_setup_broker() -> None:
     app = StartAbleApplication()
 
     with pytest.raises(AssertionError, match="You should setup a broker"):
         await app._start_broker()
+
