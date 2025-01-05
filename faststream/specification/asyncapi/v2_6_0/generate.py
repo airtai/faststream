145658e06c76ca36295496b219ab873518c7faf4
Apostol Fet <maksim170901@gmail.com>
2025-01-05 18:47:01 +0300

    add warning for overwrite channel

--- faststream/specification/asyncapi/v2_6_0/generate.py
+++ faststream/specification/asyncapi/v2_6_0/generate.py
@@ -150,16 +150,25 @@ def get_broker_channels(
     channels = {}
 
     for h in broker._subscribers:
-        # TODO: add duplication key warning
-        channels.update({
-            key: Channel.from_sub(channel) for key, channel in h.schema().items()
-        })
-
+        for key, channel in h.schema().items():
+            if key in channels:
+                warnings.warn(
+                    f"Overwrite channel handler, channels have the same names: `{key}`",
+                    RuntimeWarning,
+                    stacklevel=1,
+                )
+            channels[key] = Channel.from_sub(channel)
+             
     for p in broker._publishers:
-        # TODO: add duplication key warning
-        channels.update({
-            key: Channel.from_pub(channel) for key, channel in p.schema().items()
-        })
+        for key, channel in p.schema().items():
+            if key in channels:
+                warnings.warn(
+                    f"Overwrite channel handler, channels have the same names: `{key}`",
+                    RuntimeWarning,
+                    stacklevel=1,
+                )
+
+            channels[key] = Channel.from_pub(channel)
 
     return channels
 
