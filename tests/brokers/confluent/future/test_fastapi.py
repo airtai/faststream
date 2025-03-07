60437ce13b6613fda2d19d27a3dcb06520e952c6
Yakov-Varnaev <yakov.varnaev@gmail.com>
2025-03-07 18:35:41 +0300

    fix: use confluent base tests case class

--- tests/brokers/confluent/future/test_fastapi.py
+++ tests/brokers/confluent/future/test_fastapi.py
@@ -3,9 +3,10 @@ import pytest
 from faststream.confluent.fastapi import KafkaRouter as StreamRouter
 from faststream.confluent.router import KafkaRouter
 from tests.brokers.base.future.fastapi import FastapiTestCase
+from tests.brokers.confluent.basic import ConfluentTestcaseConfig
 
 
 @pytest.mark.confluent
-class TestRouter(FastapiTestCase):
+class TestRouter(ConfluentTestcaseConfig, FastapiTestCase):
     router_class = StreamRouter
     broker_router_class = KafkaRouter
