0db16a006c4920c72e99e60189932fc3fb9fbd40
Yakov-Varnaev <yakov.varnaev@gmail.com>
2025-03-07 18:21:27 +0300

    fix: better structure + marks

--- tests/brokers/confluent/future/test_fastapi.py
+++ tests/brokers/confluent/future/test_fastapi.py
@@ -1,8 +1,11 @@
+import pytest
+
 from faststream.confluent.fastapi import KafkaRouter as StreamRouter
 from faststream.confluent.router import KafkaRouter
 from tests.brokers.base.future import FastapiTestCase
 
 
+@pytest.mark.confluent
 class TestRouter(FastapiTestCase):
     router_class = StreamRouter
     broker_router_class = KafkaRouter
--- tests/brokers/nats/future/test_fastapi.py
+++ tests/brokers/nats/future/test_fastapi.py
@@ -1,8 +1,11 @@
+import pytest
+
 from faststream.nats.fastapi import NatsRouter as StreamRouter
 from faststream.nats.router import NatsRouter
 from tests.brokers.base.future import FastapiTestCase
 
 
+@pytest.mark.nats
 class TestRouter(FastapiTestCase):
     router_class = StreamRouter
     broker_router_class = NatsRouter
